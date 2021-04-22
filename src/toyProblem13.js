7a95a7694b4bf4b95b021a3f8a322bdd1913afe4
21grahams <graham.kirsh@gmail.com>
2021-04-21 21:14:04 -0600

    Toy Problem - doublyLinkedList - added push, pop, shift

---
+++ src/toyProblem13.js
@@ -0,0 +1,63 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = current.prev;
+      this.tail.next = null;
+      current.prev = null;
+    }
+    this.length--;
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+}
+
+var graham = new DoublyLinkedList();
+
+graham.push(1);
+graham.push(2);
+graham.push(3);
+console.log('graham: ', graham);
