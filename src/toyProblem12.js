ffea3bc42d333ad9c31f642791dd628b540f7c6d
21grahams <graham.kirsh@gmail.com>
2021-04-21 18:25:47 -0600

    Toy Problem - linkedList - Added unshift, get, set, insert

--- src/toyProblem12.js
+++ src/toyProblem12.js
@@ -1,70 +1,112 @@
-// all about linkedLists
-
 class Node {
   constructor(val) {
-      this.val = val;
-      this.next = null;
+    this.val = val;
+    this.next = null;
   }
 }
 
 class SinglyLinkedList {
   constructor() {
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
   push(val) {
-      var newNode = new Node(val)
-      if (!this.head) {
-          this.head = newNode;
-          this.tail = this.head;
-      } else {
-          this.tail.next = newNode;
-          this.tail = newNode;
-      }
-      this.length++
-      return this;
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
   }
   pop() {
     if (!this.head) {
       return undefined;
     }
-   var current = this.head;
-   var newTail = current;
-   while (current.next) {
-     newTail = current;
-     current = current.next;
-   }
-   this.tail = newTail;
-   this.tail.next = null;
-   this.length--;
-   if (this.length === 0) {
-       this.head = null;
-       this.tail = null;
-   }
-   return current;
- }
- shift() {
-     if (!this.head) return undefined;
-     let oldHead = this.head;
-     this.head = oldHead.next;
-     this.length--;
-     if (this.length === 0) {
-         this.tail = null;
-     }
-     return oldHead
- }
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    this.head = oldHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return oldHead;
+  }
+  unshift(val) {
+    let unshiftNode = new Node(val);
+    if (!this.head) {
+      this.head = unshiftNode;
+      this.tail = unshiftNode;
+    } else {
+      unshiftNode.next = this.head;
+      this.head = unshiftNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    var counter = 0;
+    var current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index, val) {
+    var foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index >= this.length) return null;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
 }
 
 var nodes = new SinglyLinkedList();
 
-nodes.push('graham');
-nodes.push('philip');
-nodes.push('kirsh');
+nodes.push("graham");
+nodes.push("philip");
+nodes.push("kirsh");
 console.log('nodes: ', nodes)
-console.log('=========================')
-nodes.pop()
-console.log('nodes after pop: ', nodes)
-console.log('=========================')
-nodes.shift()
-console.log('nodes after shift: ', nodes)
+// console.log('=========================')
+// nodes.pop()
+// console.log('nodes after pop: ', nodes)
+// console.log('=========================')
+// nodes.shift()
+// console.log('nodes after shift: ', nodes)
