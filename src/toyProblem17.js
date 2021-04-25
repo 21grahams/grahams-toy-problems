2897564187cac4358179c9b01bd59c318e3b3141
21grahams <graham.kirsh@gmail.com>
2021-04-25 12:19:46 -0600

    Toy Problem - graph - added removeEdge

--- src/toyProblem17.js
+++ src/toyProblem17.js
@@ -11,11 +11,23 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2)
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
+  }
 }
 
 var graham = new Graph();
 graham.addVertex('Boulder');
 graham.addVertex('Colorado');
-console.log('graham BEFORE edges: ', graham); // Graph { adjacencyList: { Boulder: [], Colorado: [] } }
-graham.addEdge('Colorado', 'Boulder')
-console.log('graham AFTER edges: ', graham); // Graph { adjacencyList: { Boulder: ['Colorado'], Colorado: ['Boulder'] } }
+graham.addVertex('United States');
+graham.addVertex('North America');
+graham.addVertex('Western Hemisphere');
+graham.addVertex('Globe');
+console.log('graham BEFORE edges: ', graham); // Graph { adjacencyList: { Boulder: [], Colorado: [], 'United States': [], 'North America': [], 'Western Hemisphere': [], Globe: [] } }
+graham.addEdge('Colorado', 'Boulder');
+graham.addEdge('Western Hemisphere', 'Globe');
+console.log('graham AFTER edges: ', graham); // Graph { adjacencyList: { Boulder: ['Colorado'], Colorado: ['Boulder'], 'Western Hemisphere': ['Globe'], Globe: [ 'Western Hemisphere' ] } }
+graham.removeEdge('Colorado', 'Boulder');
+graham.removeEdge('Western Hemisphere', 'Globe');
+console.log('graham AFTER removeEdge: ', graham); // Graph { adjacencyList: { Boulder: [], Colorado: [], 'United States': [], 'North America': [], 'Western Hemisphere': [], Globe: [] } }
