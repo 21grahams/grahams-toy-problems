498f61c12ed978eca7c71d71552e334463338d7a
21grahams <graham.kirsh@gmail.com>
2021-05-05 10:55:57 -0600

    changed prompt to collect length of array after dropped duplicates and keeping same array verses pushing to new array

--- src/toyProblem26.js
+++ src/toyProblem26.js
@@ -1,4 +1,8 @@
-// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+/* Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+*/
 
 // input: an array of nums
 // output: an array of those nums with dropped duplicates
@@ -6,12 +10,15 @@
 // edge cases: none
 var removeDuplicates = function(nums) {
 
-  let masterArr = [];
-  let newSet = new Set(nums);
-  for (let key of newSet) {
-      masterArr.push(key)
-  }
-  return masterArr.length;
+    if (nums.length === 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1;
 };
 
 console.log(removeDuplicates([1, 1, 2])) // [1, 2]
