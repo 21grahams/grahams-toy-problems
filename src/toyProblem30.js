86783be8a2c01b7c9e785127213627d753667dea
21grahams <graham.kirsh@gmail.com>
2021-05-19 12:51:54 -0600

    fixed typo

--- src/toyProblem30.js
+++ src/toyProblem30.js
@@ -7,7 +7,7 @@
 var mergeTwoLists = function(l1, l2) {
   if (!l1) return l2;
   if (!l2) return l1;
-  const head = l1.val >= l2.val? l2 : l1;
+  const head = l1.val >= l2.val ? l2 : l1;
   let cur = head;
 
   if (l1.val >= l2.val) l2 = l2.next;
