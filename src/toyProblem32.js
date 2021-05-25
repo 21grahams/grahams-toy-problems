ad80145d47e4d313d689ef1b289f6438f43e0e08
21grahams <graham.kirsh@gmail.com>
2021-05-25 08:55:47 -0600

    added test suite

--- src/toyProblem32.js
+++ src/toyProblem32.js
@@ -8,3 +8,10 @@ You must write an algorithm with O(log n) runtime complexity
 const searchInsert = (nums, target) => {
 
 }
+
+
+console.log(searchInsert([1, 3, 5, 6], 5)) // 2
+console.log(searchInsert([1, 3, 5, 6], 2)) // 1
+console.log(searchInsert([1, 3, 5, 6], 7)) // 4
+console.log(searchInsert([1, 3, 5, 6], 0)) // 0
+console.log(searchInsert([1], 0)) // 0
