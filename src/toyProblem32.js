07fb2ec4e9ed61caedbbae5e23e2420172985981
21grahams <graham.kirsh@gmail.com>
2021-05-25 09:14:29 -0600

    added pseudocode

--- src/toyProblem32.js
+++ src/toyProblem32.js
@@ -5,13 +5,20 @@ Given a sorted array of distinct integers and a target value, return the index i
 You must write an algorithm with O(log n) runtime complexity
 */
 
+// input: a sorted array of nums, and a target num
+// output: a num, which is the index of the target number
+// constraints: linear time complexity
+// edge cases: if target value doesn't exist in array of nums, return the position it would be in
 const searchInsert = (nums, target) => {
+  // loop over nums
+  // if target equals currentNum
+  // return index
+  // else if target is greater than currentNum && less than currentNum plus one
+  // return index
+};
 
-}
-
-
-console.log(searchInsert([1, 3, 5, 6], 5)) // 2
-console.log(searchInsert([1, 3, 5, 6], 2)) // 1
-console.log(searchInsert([1, 3, 5, 6], 7)) // 4
-console.log(searchInsert([1, 3, 5, 6], 0)) // 0
-console.log(searchInsert([1], 0)) // 0
+console.log(searchInsert([1, 3, 5, 6], 5)); // 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // 4
+console.log(searchInsert([1, 3, 5, 6], 0)); // 0
+console.log(searchInsert([1], 0)); // 0
