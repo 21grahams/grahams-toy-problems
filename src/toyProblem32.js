56cdbb4864177a0db3c8c6477a024cda378df697
21grahams <graham.kirsh@gmail.com>
2021-05-25 09:55:10 -0600

    attempt 2 - incomplete

--- src/toyProblem32.js
+++ src/toyProblem32.js
@@ -20,13 +20,19 @@ const searchInsert = (nums, target) => {
     if (target === nums[i]) {
       return i;
     } else if (target > nums[i] && target < nums[i + 1]) {
-      return i;
+        nums.splice(nums[i], 0, target)
+        console.log('nums: ', nums)
+        return nums[i];
+    } else if (target > nums[i]) {
+      nums.push(target);
+      return nums[i];
     }
   }
 };
 
-console.log(searchInsert([1, 3, 5, 6], 5)); // 2
-console.log(searchInsert([1, 3, 5, 6], 2)); // 1
-console.log(searchInsert([1, 3, 5, 6], 7)); // 4
-console.log(searchInsert([1, 3, 5, 6], 0)); // 0
-console.log(searchInsert([1], 0)); // 0
+// console.log(searchInsert([1, 3, 5, 6], 5)); // 2
+// console.log(searchInsert([1, 3, 5, 6], 2)); // 1
+// console.log(searchInsert([1, 3, 5, 6], 7)); // 4
+// console.log(searchInsert([1, 3, 5, 6], 0)); // 0
+// console.log(searchInsert([1], 0)); // 0
+console.log(searchInsert([1, 3, 5], 4)); // 2
