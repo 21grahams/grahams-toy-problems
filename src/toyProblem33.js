148c7f1e3f05479ef4a1cd072a2a14a9ab067564
21grahams <graham.kirsh@gmail.com>
2021-05-28 08:42:48 -0600

    pseudocode added along with IOCE

--- src/toyProblem33.js
+++ src/toyProblem33.js
@@ -4,9 +4,16 @@ Given a string s consists of some words separated by spaces, return the length o
 
 A word is a maximal substring consisting of non-space characters only */
 
-const lengthOfLastWord () => {
-  // do something
-}
+// input: a word or set of words in a string
+// output: a number, that represents the length of the last word in the sentence
+// constraints: only english letters and normal spaces between words
+// edge cases: if last word doesn't exist, return 0. A word is a maximal substring consisting of non-space characters only.
+const lengthOfLastWord = s => {
+  // account for edge case
+
+  // loop over the string starting from the end
+    // collect the length of that last word and return
+};
 
 console.log(lengthOfLastWord('Hello World')) // 5
 console.log(lengthOfLastWord('')) // 0
