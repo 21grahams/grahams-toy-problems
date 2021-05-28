9148acd37ea7837cbe36e5886a62de2bd6b816eb
21grahams <graham.kirsh@gmail.com>
2021-05-28 09:26:00 -0600

    problem complete

--- src/toyProblem33.js
+++ src/toyProblem33.js
@@ -8,12 +8,16 @@ A word is a maximal substring consisting of non-space characters only */
 // output: a number, that represents the length of the last word in the sentence
 // constraints: only english letters and normal spaces between words
 // edge cases: if last word doesn't exist, return 0. A word is a maximal substring consisting of non-space characters only.
-const lengthOfLastWord = s => {
-  // account for edge case
+const lengthOfLastWord = (s) => {
 
-  // loop over the string starting from the end
-    // collect the length of that last word and return
+  let wordArray = s.trim().split(" ");
+  if (s.length === 0) {
+    return 0;
+  } else {
+    return wordArray[wordArray.length - 1].length;
+  }
 };
 
-console.log(lengthOfLastWord('Hello World')) // 5
-console.log(lengthOfLastWord('')) // 0
+console.log(lengthOfLastWord("Hello World")); // 5
+console.log(lengthOfLastWord("")); // 0
+console.log(lengthOfLastWord("a")); // 1
