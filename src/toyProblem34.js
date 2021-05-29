b6aaa1beac6907e57a53b93100e40707b17f2448
21grahams <graham.kirsh@gmail.com>
2021-05-29 17:06:19 -0600

    leet code problem skeleton and test suite built out

---
+++ src/toyProblem34.js
@@ -0,0 +1,16 @@
+// leetcode - Merge Sorted Arrays
+
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+*/
+
+const merge = (nums1, m, nums2, n) => {
+  // do something
+};
+
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+
+console.log(merge([1], 1, [], 0));
