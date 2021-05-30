cf85d2baa3d79b6d5dbcee0e98eee7fa40b3dabd
21grahams <graham.kirsh@gmail.com>
2021-05-30 13:28:27 -0600

    attempt 1. Passing tests but not passing leetcode

--- src/toyProblem34.js
+++ src/toyProblem34.js
@@ -13,15 +13,26 @@ The number of elements initialized in nums1 and nums2 are m and n respectively. 
 
 const merge = (nums1, m, nums2, n) => {
   // account for edge case
-
+  if (m === 0 && n === 0) return [];
   // slice nums1 and nums2 by m and n
+  if (m === 0) {
+    nums1 = nums1.length = [];
+    return nums1.push(nums2);
+  } else {
+    nums1.splice(m, m);
+  }
+  nums2.splice(n, n);
   // splice in nums2 to num1
+  nums1.push(...nums2);
+  console.log("nums1: ", nums1);
   // return sort of nums1
+  return nums1.sort((a, b) => {
+    return a - b;
+  });
 };
-
 
 console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1,2,2,3,5,6]
 
 console.log(merge([1], 1, [], 0)); // [1]
 
-console.log(merge([0], 0, [1], 1)) // [1]
+console.log(merge([0], 0, [1], 1)); // [1]
