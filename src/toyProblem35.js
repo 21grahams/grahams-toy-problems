260643ac71b8f43b004ad9128e296e251b1dae3b
21grahams <graham.kirsh@gmail.com>
2021-05-31 11:56:15 -0600

    proposed pseudocode to begin coding

--- src/toyProblem35.js
+++ src/toyProblem35.js
@@ -4,10 +4,22 @@ Maximum Subarray
 
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum */
 
-const maxSubArray = nums => {
+const maxSubArray = (nums) => {
 
-}
+  // account for edge cases
 
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) // 6
-console.log(maxSubArray([1])) // 1
-console.log(maxSubArray([5,4,-1,7,8])) // 23
+  // create a count
+  // create a maxCountSoFar
+
+  // loop over nums array
+    // add the current element to count
+    // if the count is larger than maxCountSoFar
+      // set MaxCountSoFar to count
+
+  // return count
+
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+console.log(maxSubArray([1])); // 1
+console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
