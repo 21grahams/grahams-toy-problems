ad171c1e4940c5b94ae6dc00c33dce09827f1b01
21grahams <graham.kirsh@gmail.com>
2021-05-31 12:40:01 -0600

    solution was very close. Had to spread operator with Math.max to max variable to account for arrays with only negative numbers

--- src/toyProblem35.js
+++ src/toyProblem35.js
@@ -12,7 +12,7 @@ const maxSubArray = (nums) => {
   // create a count
   let count = 0;
   // create a maxCountSoFar
-  let maxCountSoFar = 0;
+  let maxCountSoFar = Math.max(...nums);
   // loop over nums array
   for (let i = 0; i < nums.length; i++) {
     // add the current element to count
@@ -35,3 +35,4 @@ const maxSubArray = (nums) => {
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
 console.log(maxSubArray([1])); // 1
 console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
+console.log(maxSubArray([-2, -1])); // -1
