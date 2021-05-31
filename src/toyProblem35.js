832fdfef65c217a795ae6a8cdcc630c95d10b258
21grahams <graham.kirsh@gmail.com>
2021-05-31 11:08:53 -0600

    leetcode - Maximum Subarray created and test suite built

---
+++ src/toyProblem35.js
@@ -0,0 +1,13 @@
+/* leetcode problem
+
+Maximum Subarray
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum */
+
+const maxSubArray = nums => {
+
+}
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) // 6
+console.log(maxSubArray([1])) // 1
+console.log(maxSubArray([5,4,-1,7,8])) // 23
