9a995e9a2fb3a5291edbb0b93e28d7bf464ea964
21grahams <graham.kirsh@gmail.com>
2021-06-01 09:50:39 -0600

    leetcode solution finalized and add'l test cases added

--- src/toyProblem36.js
+++ src/toyProblem36.js
@@ -11,16 +11,25 @@ You may assume the integer does not contain any leading zero, except the number 
 // constraints: none at this time
 // edge cases: return empty array if input is empty
 const plusOne = (digits) => {
-  // account for edge case
+  let nines = true;
   if (digits.length === 0) return [];
-
-  // collect the last number of the digits array
-  // increment by one
-  digits[digits.length - 1]++;
-  // return digits
+  if (digits.length === 1 && digits[0] === 9) return [1, 0];
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] > 8) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      nines = false;
+      break;
+    }
+  }
+  if (nines) digits.unshift(1);
   return digits;
 };
 
 console.log(plusOne([1, 2, 3])); // [1, 2, 4]
 console.log(plusOne([4, 3, 2, 1])); // [4, 3, 2, 2]
 console.log(plusOne([0])); // [1]
+console.log(plusOne([9])); // [1, 0]
+console.log(plusOne([9, 9])); // [1, 0, 0]
+console.log(plusOne([9, 9, 9])); // [1, 0, 0, 0]
