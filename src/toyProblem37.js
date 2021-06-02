02272f584f6666e977921b6378e5ec4d5ea9eb0b
21grahams <graham.kirsh@gmail.com>
2021-06-02 14:10:48 -0600

    Pseudocode implemented. Likely need to refactor

--- src/toyProblem37.js
+++ src/toyProblem37.js
@@ -9,8 +9,20 @@ Given two binary strings a and b, return their sum as a binary string */
 // constraints: nothing at this time
 // edge cases: return empty string if both arguments are empty
 const addBinary = (a, b) => {
-  // do something
-}
+  // account for edge case
+
+  // create a master string
+  // split into individual nums
+  // loop in descending order
+      // if current element of string a is 0 && current element of string b is 0
+          // continue
+  // if current element of string a is 1 && current element of string b is 0 ||current element of string b is 1 && current element of string a is 0
+    // numArray = 1
+  // else if (current element of string a is 1 && current element of string b is 1 && (current element minus 1 of string a === undefined && current element minus 1 of string b === undefined)
+    // numArray = 10
+  // else if (current element of string a is 1 && current element of string b is 1 && (current element minus 1 of string a !== undefined || current element minus 1 of string b === undefined))
+  // numArray = 0
+};
 
 // binary  1  2  4  8  16  32   64   128
 
@@ -23,6 +35,11 @@ const addBinary = (a, b) => {
 // b - 1011
 //__________
 //     10101
+
+// a = ['1', '1']
+// b = ['1']
+// i (for a) = 0
+// i (for b) = 0
 
 console.log('11', '1') // '100'
 console.log('1010', '1011') // '10101'
