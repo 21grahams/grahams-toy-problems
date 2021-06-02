d9b4bfc78f770e7cb94d4ab26f01de8a0b2cc5b9
21grahams <graham.kirsh@gmail.com>
2021-06-02 12:59:44 -0600

    leetcode problem - Add Binary - init and test suite built

---
+++ src/toyProblem37.js
@@ -0,0 +1,12 @@
+/* leetcode problem
+
+Add Binary
+
+Given two binary strings a and b, return their sum as a binary string */
+
+const addBinary = (a, b) => {
+  // do something
+}
+
+console.log('11', '1') // '100'
+console.log('1010', '1011') // '10101'
