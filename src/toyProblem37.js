593720373abc6c43372114fcfb692934ffbbff87
21grahams <graham.kirsh@gmail.com>
2021-06-02 13:44:32 -0600

    diagram and IOCE built. Working on pseudocode

--- src/toyProblem37.js
+++ src/toyProblem37.js
@@ -4,9 +4,25 @@ Add Binary
 
 Given two binary strings a and b, return their sum as a binary string */
 
+// input: two arguments, which are each strings containing binary nums
+// output: the sum of these two binary nums as a string
+// constraints: nothing at this time
+// edge cases: return empty string if both arguments are empty
 const addBinary = (a, b) => {
   // do something
 }
+
+// binary  1  2  4  8  16  32   64   128
+
+// a - 11
+// b -  1
+//________
+//     100
+
+// a - 1010
+// b - 1011
+//__________
+//     10101
 
 console.log('11', '1') // '100'
 console.log('1010', '1011') // '10101'
