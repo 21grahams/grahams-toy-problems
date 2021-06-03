cf92fa76701259a5d04729af6ecd278ad0c0b0f1
21grahams <graham.kirsh@gmail.com>
2021-06-03 14:04:01 -0600

    IOCE and pseudocode init

--- src/toyProblem38.js
+++ src/toyProblem38.js
@@ -6,8 +6,26 @@ You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? */
 
+// input: n, which represents a step or a number of steps to reach the top
+// output: the numbers of ways to climb to the top of the staircase
+// constraints: can only climb 1 or 2 steps at a time
+// edge cases: if n is 0, return 0
 const climbStairs = n => {
-  // do something
+  // account for edge case
+
+  // create a count
+  // base case recursion
+  // if n equals one
+    // return one
+
+  // create a var for step one
+  // create a var for step two
+  // create a var for the combo of both
+  // create an outer function which we'll invoke below
+
+    // loop over and invoke inner function with n getting added to count
+
+  // return count
 };
 
 console.log(climbStairs(2)) // 2
