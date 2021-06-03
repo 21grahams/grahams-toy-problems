ef62e3cc56606771357f11a00d40f8a655c356d0
21grahams <graham.kirsh@gmail.com>
2021-06-03 13:24:15 -0600

    leetcode problem - Climbing Stairs - init and test suite built

---
+++ src/toyProblem38.js
@@ -0,0 +1,14 @@
+/* leetcode problem
+
+Climbing Stairs
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? */
+
+const climbStairs = n => {
+  // do something
+};
+
+console.log(climbStairs(2)) // 2
+console.log(climbStairs(3)) // 3
