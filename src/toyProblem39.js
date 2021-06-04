af89a22c53b991afdc57dcf11d9f5628ebf15c7d
21grahams <graham.kirsh@gmail.com>
2021-06-04 08:31:55 -0600

    fixed quick typo in title

--- src/toyProblem39.js
+++ src/toyProblem39.js
@@ -1,6 +1,6 @@
 /* leetcode problem
 
-Remove Sorted Duplicates from Sorted List
+Remove Duplicates from Sorted List
 
 Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well
 */
