9129ca08227e39be1775417e82531b2a30a66ecc
21grahams <graham.kirsh@gmail.com>
2021-06-04 11:52:13 -0600

    initial attempt. passes first test, not second

--- src/toyProblem39.js
+++ src/toyProblem39.js
@@ -5,18 +5,29 @@ Remove Duplicates from Sorted List
 Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well
 */
 
-
 // input: a singly linked list that could and likely will have duplicate values
 // output: a sorted singly linked list that has no duplicate values
 // constraints: list is guaranteed to already be sorted in ascending order
 // edge cases: nothing at this time
-const deleteDuplicates = head => {
-
-  // access each node in the linkedList
-  // if the current node matches the next node in the linked list
-    // remove it
-  // return linkedList
+function ListNode(val, next) {
+   this.val = (val===undefined ? 0 : val)
+   this.next = (next===undefined ? null : next)
 };
 
-console.log(deleteDuplicates([1, 1, 2])) // [1, 2]
-console.log(deleteDuplicates([1, 1, 2, 3, 3])) // [1, 2, 3]
+const deleteDuplicates = (head) => {
+
+  var length = 0;
+  if (!head) {
+      return undefined;
+  }
+  let current = head;
+  let newHead = head.next;
+      if (current.next.val === current.val) {
+          current = newHead;
+          head = current;
+      }
+  return head;
+};
+
+console.log(deleteDuplicates([1, 1, 2])); // [1, 2]
+console.log(deleteDuplicates([1, 1, 2, 3, 3])); // [1, 2, 3]
