76f3349e1135b8a3b032d7c21a55aa7a878aa593
21grahams <graham.kirsh@gmail.com>
2021-06-06 10:39:55 -0600

    tests passing on leetcode

--- src/toyProblem39.js
+++ src/toyProblem39.js
@@ -10,26 +10,34 @@ Given the head of a sorted linked list, delete all duplicates such that each ele
 // constraints: list is guaranteed to already be sorted in ascending order
 // edge cases: nothing at this time
 function ListNode(val, next) {
-   this.val = (val===undefined ? 0 : val)
-   this.next = (next===undefined ? null : next)
-};
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
 
 const deleteDuplicates = (head) => {
-    // create two references to head
-    // temp will iterate through entire linked list
-    // prev will point towards the first occurrence of every element
+  // create two references to head
+  // temp will iterate through entire linked list
+  // prev will point towards the first occurrence of every element
+  var temp = head,
+    prev = head;
 
-    // traverse list until the last node
-
+  // traverse list until the last node
+  while (temp !== null) {
     // compare values of both pointers
-
     // if the value of previous is not equal to the value of temp, that means there are no more occurrences of the prev val. So set next of prev to temp node
-
+    if (prev.val !== temp.val) {
+      prev.next = temp;
+      prev = temp;
+    }
     // Set the temp to the next node
-
-    // Create edge case if there are more than one occurrence of the last element
-
-    // return linkedList
+    temp = temp.next;
+  }
+  // Create edge case if there are more than one occurrence of the last element
+  if (prev !== temp) {
+    prev.next = null;
+  }
+  // return linkedList
+  return head;
 };
 
 console.log(deleteDuplicates([1, 1, 2])); // [1, 2]
