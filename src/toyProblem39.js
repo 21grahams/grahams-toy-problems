33bcb52707e5a09b1426198cddc5968055a27161
21grahams <graham.kirsh@gmail.com>
2021-06-06 10:52:00 -0600

    removed pseudocode, cleaned up code

--- src/toyProblem39.js
+++ src/toyProblem39.js
@@ -15,28 +15,18 @@ function ListNode(val, next) {
 }
 
 const deleteDuplicates = (head) => {
-  // create two references to head
-  // temp will iterate through entire linked list
-  // prev will point towards the first occurrence of every element
-  var temp = head,
-    prev = head;
 
-  // traverse list until the last node
-  while (temp !== null) {
-    // compare values of both pointers
-    // if the value of previous is not equal to the value of temp, that means there are no more occurrences of the prev val. So set next of prev to temp node
-    if (prev.val !== temp.val) {
-      prev.next = temp;
-      prev = temp;
+
+  let current = head;
+  while (current && current.next) {
+
+    if (current.val === current.next.val) {
+      let nextPlace = current.next;
+      current.next = nextPlace.next;
+    } else {
+        current = current.next
     }
-    // Set the temp to the next node
-    temp = temp.next;
   }
-  // Create edge case if there are more than one occurrence of the last element
-  if (prev !== temp) {
-    prev.next = null;
-  }
-  // return linkedList
   return head;
 };
 
