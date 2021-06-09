94a5cdd38aa87dd86d3ba7a9b3739cd6db1c2541
21grahams <graham.kirsh@gmail.com>
2021-06-09 07:01:03 -0600

    IOCE and pseudocode init

--- src/toyProblem40.js
+++ src/toyProblem40.js
@@ -5,8 +5,28 @@ Given the roots of two binary trees p and q, write a function to check if they a
 Two binary trees are considered the same if they are structurally identical, and the nodes have the same value
 */
 
+// input: a set of two different binary trees
+// output: a boolean. True if the BTs match each other and false if not
+// constraints: none at this time
+// edge cases: none at this time
 const isSameTree = (p, q) => {
-  // do something
+  // QUICKER VERSION (might need to change to LONGER version)
+  // loop through both trees
+    // if each node in tree p matches each node in tree q
+      // return true
+    // otherwise
+      // return false
+
+  // LONGER VERSION
+
+  // create a queue (with an array) and a variable that will store each node of the tree
+  // place root node in the queue
+  // loop as long as there is anything in the queue
+    // if node at left exists in both tree q and p
+      // if node at right exists in both tree q and p
+        // if these nodes equal each other
+          // return true
+  // return false
 };
 
 console.log(isSameTree([1, 2, 3], [1, 2, 3])) // true
