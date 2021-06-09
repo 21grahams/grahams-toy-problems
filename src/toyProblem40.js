69760dcd431038cebee2a10ed54dbd2e9c94cbd1
21grahams <graham.kirsh@gmail.com>
2021-06-09 07:37:55 -0600

    first attempt. Passing several leetcode test suites, but not anything with null values

--- src/toyProblem40.js
+++ src/toyProblem40.js
@@ -12,23 +12,50 @@ Two binary trees are considered the same if they are structurally identical, and
 const isSameTree = (p, q) => {
   // QUICKER VERSION (might need to change to LONGER version)
   // loop through both trees
-    // if each node in tree p matches each node in tree q
-      // return true
-    // otherwise
-      // return false
+  // if each node in tree p matches each node in tree q
+  // return true
+  // otherwise
+  // return false
+  let masterObj = {};
+  let masterObj2 = {};
+  for (let key in p) {
+    let val = p[key];
+    masterObj[key] = val;
+  }
+  for (let key in q) {
+    let val2 = q[key];
+    masterObj2[key] = val2;
+  }
+  if (masterObj && masterObj2 && masterObj.val === masterObj2.val) {
+    if (
+      masterObj.left.val &&
+      masterObj2.left.val &&
+      masterObj.left.val === masterObj2.left.val
+    ) {
+      if (
+        (masterObj.right.val &&
+          masterObj2.right.val &&
+          masterObj.right.val === masterObj2.right.val) ||
+        (masterObj.right === null && masterObj2.right === null)
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
 
   // LONGER VERSION
 
   // create a queue (with an array) and a variable that will store each node of the tree
   // place root node in the queue
   // loop as long as there is anything in the queue
-    // if node at left exists in both tree q and p
-      // if node at right exists in both tree q and p
-        // if these nodes equal each other
-          // return true
+  // if node at left exists in both tree q and p
+  // if node at right exists in both tree q and p
+  // if these nodes equal each other
+  // return true
   // return false
 };
 
-console.log(isSameTree([1, 2, 3], [1, 2, 3])) // true
-console.log(isSameTree([1, 2], [1, null, 2])) // false
-console.log(isSameTree([1, 2, 1], [1, 1, 2])) // false
+console.log(isSameTree([1, 2, 3], [1, 2, 3])); // true
+console.log(isSameTree([1, 2], [1, null, 2])); // false
+console.log(isSameTree([1, 2, 1], [1, 1, 2])); // false
