e7c031ef60fada69343d2472650e33a11872353b
21grahams <graham.kirsh@gmail.com>
2021-06-15 08:40:29 -0600

    leetcode problem - isSymmetric init

---
+++ src/toyProblem41.js
@@ -0,0 +1,11 @@
+/* leetcode problem - Symmetric Tree
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center)
+*/
+
+const isSymmetric = root => {
+  // do something
+};
+
+console.log(isSymmetric([1, 2, 2, 3, 4, 4, 3])) // true
+console.log(isSymmetric([1, 2, 2, null, 3, null, 3])) // false
