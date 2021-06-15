b22c38c9ede4c4959be91243fb968ef2af28916a
21grahams <graham.kirsh@gmail.com>
2021-06-15 08:52:12 -0600

    initial pseudocode and IOCE implemented

--- src/toyProblem41.js
+++ src/toyProblem41.js
@@ -3,8 +3,19 @@
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center)
 */
 
+// input: a binary tree
+// output: a bool. True if the left side of tree matches the right side, false if not
+// constraints: none currently
+// edge cases: if no tree exists, return false
 const isSymmetric = root => {
-  // do something
+  // handle edge case
+
+  // if left node doesn't equal right node
+  // return false
+  // otherwise
+  // return true
+
+  // recursively call left node and right node and compare
 };
 
 console.log(isSymmetric([1, 2, 2, 3, 4, 4, 3])) // true
