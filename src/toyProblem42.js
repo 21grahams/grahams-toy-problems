84094d1a340efeb28eb82d4f0ba0f0c706954012
21grahams <graham.kirsh@gmail.com>
2021-06-16 08:39:58 -0600

    leetcode init - Maximum Depth of Binary Tree

---
+++ src/toyProblem42.js
@@ -0,0 +1,14 @@
+/* leetcode - Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node
+*/
+
+const maxDepth = root => {
+  // do something
+};
+
+console.log(maxDepth([3, 9, 20, null, null, 15, 7])) // 3
+console.log(maxDepth([1, null, 1])) // 2
+console.log(maxDepth([])) // 0
