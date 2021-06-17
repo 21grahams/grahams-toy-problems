41a1e48d3c642c061ce5e8e6d866d13344e79e06
21grahams <graham.kirsh@gmail.com>
2021-06-17 08:58:42 -0600

    attempt one

--- src/toyProblem43.js
+++ src/toyProblem43.js
@@ -9,28 +9,31 @@ A height-balanced binary tree is a binary tree in which the depth of the two sub
 // output: a height-balanced binary search tree
 // constraints: the depth of the two subtrees of every node never differs by more than one
 // edge cases: if array is empty, return empty BST
-const sortedArrayToBST = nums => {
-  // account for the edge case
+const sortedArrayToBST = (nums) => {
+  if (nums.length === []) return [];
 
-  // create a rootNum
-  // loop through nums array, collect rootNum, which is middle num
+  let rootNum;
+  rootNum = nums[Math.floor((nums.length - 1) / 2)];
 
-  // loop again through nums array
-    // if currentNum is less than rootNum
-      // store it to the left of root
-    // otherwise
-      // store it to the right of root
-    // if currentNum is less than rootNum but greater than root.left
-      // store to the right of root.left
-    // otherwise
-      // store to the left of root.left
-    // if currentNum is greater than rootNum but less than root.right
-      // store it to the left of root.right
-    // otherwise
-      // store it to the right of root.right
-
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < rootNum) {
+      rootNum.left.val = nums[i];
+    } else {
+      rootNum.right.val = nums[i];
+    }
+    if (nums[i] < rootNum && nums[i] > rootNum.left.val) {
+      rootNum.left.right.val = nums[i];
+    } else {
+      rootNum.left.left.val = nums[i];
+    }
+    if (nums[i] > rootNum && nums[i] < rootNum.right.val) {
+      rootNum.right.left.val = nums[i];
+    } else {
+      rootNum.right.right.val = nums[i];
+    }
+  }
+  return rootNum;
 };
-
 
 // [1, 2, 3, 4, 5, 6]
 
@@ -40,5 +43,5 @@ const sortedArrayToBST = nums => {
 // null    3
 // Math.floor(length - 1) / 2 = middle number
 
-console.log(sortedArrayToBST([-10, -3, 0, 5, 9])) // [0, -3, 9, -10, null, 5] ||| [0, -10, 5, null, -3,null, 9] is also accepted
-console.log(sortedArrayToBST([1, 3])) // [3, 1]
+console.log(sortedArrayToBST([-10, -3, 0, 5, 9])); // [0, -3, 9, -10, null, 5] ||| [0, -10, 5, null, -3,null, 9] is also accepted
+console.log(sortedArrayToBST([1, 3])); // [3, 1]
