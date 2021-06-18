6ade470bb671af832989b313e63a5d28d3c8ad21
21grahams <graham.kirsh@gmail.com>
2021-06-18 09:03:53 -0600

    problem solved

--- src/toyProblem44.js
+++ src/toyProblem44.js
@@ -4,18 +4,33 @@
 // output: an object containing any duplicate letters. The letter is the key and the number of occurences is the value. Must be a letter occuring less than the target number
 // constraints: none at this time
 // edge cases: none of importance
-const countDupes = (letters, target) {
+const countDupes = (letters, target) => {
   // create an object
-  // create a 'seen' variable, assign to false
+  let initialObj = {};
+  let masterObj = {};
   // create a count, start at 0
+  let count = 0;
   // loop over the letters string
+  for (let i = 0; i < letters.length; i++) {
     // if current letter is undefined
+    if (initialObj[letters[i]] === undefined) {
       // assign to one
-    // otherwise
+      initialObj[letters[i]] = 1;
+      // otherwise
+    } else {
       // increment by one
-
+      initialObj[letters[i]]++;
+    }
+  }
   // if the value of the letter in the object is less than target number
+  for (let key in initialObj) {
+    let value = initialObj[key];
+    if (value < target && value > 1) {
+      masterObj[key] = value;
+    }
+  }
     // return that object
+    return masterObj;
 };
 
 
