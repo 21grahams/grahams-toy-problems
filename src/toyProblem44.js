200d2f56f5180fe013e3fb7058fc3751f6c5a9ba
21grahams <graham.kirsh@gmail.com>
2021-06-18 08:54:20 -0600

    IOCE and pseudocode implemented

--- src/toyProblem44.js
+++ src/toyProblem44.js
@@ -1,7 +1,21 @@
 /* Given a string of letters and a target number, return an object representing any duplicate letters as the key and the amount of repetitions as the value that are LESS THAN the target number */
 
+// input: a string of letters and a target number
+// output: an object containing any duplicate letters. The letter is the key and the number of occurences is the value. Must be a letter occuring less than the target number
+// constraints: none at this time
+// edge cases: none of importance
 const countDupes = (letters, target) {
-  // YOUR CODE HERE
+  // create an object
+  // create a 'seen' variable, assign to false
+  // create a count, start at 0
+  // loop over the letters string
+    // if current letter is undefined
+      // assign to one
+    // otherwise
+      // increment by one
+
+  // if the value of the letter in the object is less than target number
+    // return that object
 };
 
 
