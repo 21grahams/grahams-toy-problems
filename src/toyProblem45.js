e7bf69e13ac2ce530f1d929d514c78f90c92d1bc
21grahams <graham.kirsh@gmail.com>
2021-06-21 09:29:54 -0600

    recursive approach solution

--- src/toyProblem45.js
+++ src/toyProblem45.js
@@ -11,23 +11,30 @@ a binary tree in which the left and right subtrees of every node differ in heigh
 // output: a bool, depending on if the binary tree is height balanced. True if so, false if not
 // contraints: Only constraint is the constraint of what a height balanced tree is. Child subtree must only differ in height by no more than one
 // edge cases: if root is empty, return true
-const isBalanced = root => {
-  // account for edge case
+const checkHeight = (node) => {
+  if (node === null) return 0;
 
-  // create a node value, set to root
-  // create a queue value, set to [root]
-  // while the root exists
-    // create a cur value, set to queue.length
-      // iterate over cur
-        // set node to shifting first val out of queue
-        // if left node and right node exist
-          // push them into the queue
-        // else
-          // return true
+  const left = checkHeight(node.left);
+  const right = checkHeight(node.right);
 
-  // return false
+  if (
+    // if a previous call has returned false,
+    // we need to pass false all the way up
+    left === false ||
+    right === false ||
+    Math.abs(left - right) > 1
+  ) {
+    return false;
+  }
+
+  // height of a node
+  return Math.max(left, right) + 1;
+};
+const isBalanced = (root) => {
+  if (!root) return true;
+  return checkHeight(root) !== false;
 };
 
-console.log(root([3, 9, 20, null, null, 15, 7])) // true
-console.log(root([1, 2, 2, 3, 3, null, null, 4, 4])) // false
-console.log(root([])) // true
+console.log(root([3, 9, 20, null, null, 15, 7])); // true
+console.log(root([1, 2, 2, 3, 3, null, null, 4, 4])); // false
+console.log(root([])); // true
