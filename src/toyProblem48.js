41614dc9eefa073ce48bc7ecea934f54bd6987a5
21grahams <graham.kirsh@gmail.com>
2021-06-25 08:24:53 -0600

    solution completed with recursion and runtime included

--- src/toyProblem48.js
+++ src/toyProblem48.js
@@ -4,17 +4,28 @@ Given the root of a binary tree and an integer targetSum, return true if the tre
 
 A leaf is a node with no children */
 
- // input: a binary tree containing a bunch of nodes and children
- // output: a bool. True if the sum from root to leaf equals targetSum, false if not
- // constraints: The number of nodes in the tree is in the range [0, 5000]
- // edge cases: return false if tree is empty, return true if root equals target sum
- const hasPathSum = (root, targetSum) => {
+// input: a binary tree containing a bunch of nodes and children
+// output: a bool. True if the sum from root to leaf equals targetSum, false if not
+// constraints: The number of nodes in the tree is in the range [0, 5000]
+// edge cases: return false if tree is empty, return true if root equals target sum
+const hasPathSum = (root, targetSum) => {
   // account for edge cases
+  if (!root) return false;
+  if (root.val === targetSum && root.left === null && root.right === null)
+    return true;
 
   // recursively call function on left side of tree and subtract sum from the root value OR recursively call function on right side of tree and subtract sum from the root value
-
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
 };
 
-console.log(hasPathSum([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1], 22)) // true
-console.log(hasPathSum([1, 2, 3], 5)) // false
-console.log(hasPathSum([1, 2], 0)) // false
+// Runtime: 84 ms, faster than 87.46% of JavaScript online submissions for Path Sum.
+// Memory Usage: 42.9 MB, less than 10.55% of JavaScript online submissions for Path Sum
+
+console.log(
+  hasPathSum([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1], 22)
+); // true
+console.log(hasPathSum([1, 2, 3], 5)); // false
+console.log(hasPathSum([1, 2], 0)); // false
