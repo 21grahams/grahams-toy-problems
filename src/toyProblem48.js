7c6b3b414798ff4cfb3ea74ef56ed35bf9a99121
21grahams <graham.kirsh@gmail.com>
2021-06-25 07:59:43 -0600

    leetcode problem - Path Sum initialized

---
+++ src/toyProblem48.js
@@ -0,0 +1,13 @@
+/* leetcode - Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children */
+
+const hasPathSum = (root, targetSum) => {
+  // do something
+};
+
+console.log(hasPathSum([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1], 22)) // true
+console.log(hasPathSum([1, 2, 3], 5)) // false
+console.log(hasPathSum([1, 2], 0)) // false
