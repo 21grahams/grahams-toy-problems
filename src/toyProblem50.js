5e4242f6fb803906a9fc39ca2f302d5cc07f8bfb
21grahams <graham.kirsh@gmail.com>
2021-06-28 07:58:32 -0600

    leetcode - Reverse Integer

---
+++ src/toyProblem50.js
@@ -0,0 +1,14 @@
+/* leetcode - Reverse Integer
+
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned) */
+
+const reverse = x => {
+  // do something
+};
+
+console.log(reverse(123)) // 321
+console.log(reverse(-123)) // -321
+console.log(reverse(120)) // 21
+console.log(reverse(0)) // 0
