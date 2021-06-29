469c18ee09a294bb367f989425619c0aaaf90d97
21grahams <graham.kirsh@gmail.com>
2021-06-29 09:17:27 -0600

    solution implemented

--- src/toyProblem51.js
+++ src/toyProblem51.js
@@ -12,24 +12,23 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 // output: a single number which is the maximum amount of profit you can achieve from that transaction
 // constraints: 1 <= prices.length <= 10^5. 0 <= prices[i] <= 10^4
 // edge cases: If you cannot get any profit, return 0. If the array is empty, return null
-const maxProfit = prices => {
-  // account for the edge cases
+const maxProfit = (prices) => {
+  let less = prices[0];
+  let profit = 0;
 
-  // create a totalProfit number
-  // create a var for max number
-  // create a var for min number
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < less) {
+      less = prices[i];
+    }
+    if (prices[i] - less > profit) {
+      profit = prices[i] - less;
+    }
+  }
 
-  // loop over the prices array
-    // collect the min number and store
-    // collect the max number and store
-
-  // if the min number preceeds the max number
-    // subtract the min number from the max number and store in totalProfit number
-
-  // return totalProfit number
+  return profit;
 };
 
-console.log(prices([7, 1, 5, 3, 6, 4])) // 5
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
 // explanation - Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5. Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-console.log(prices([7, 6, 4, 3, 1])) // 0// explanation - In this case, no transactions are done and the max profit is 0
+console.log(maxProfit([7, 6, 4, 3, 1])); // 0// explanation - In this case, no transactions are done and the max profit is 0
