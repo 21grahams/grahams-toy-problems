0f7a5c30f37e34ebd034527c4164b32ceeedf1a8
21grahams <graham.kirsh@gmail.com>
2021-07-01 11:30:41 -0600

    leetcode problem - Valid Palindrome

---
+++ src/toyProblem53.js
@@ -0,0 +1,11 @@
+/* Leetcode problem - Valid Palindrome
+
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases */
+
+const isPalindrome = s => {
+  // do something
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")) // true
+
+console.log(isPalindrome("race a car")) // false
