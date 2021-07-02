c0f36fea15aaa4ae7de09645796c5f142f3f7e92
21grahams <graham.kirsh@gmail.com>
2021-07-02 07:57:43 -0600

    leetcode problem - Single Number

---
+++ src/toyProblem54.js
@@ -0,0 +1,15 @@
+/* leetcode Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space */
+
+const singleNumber = nums => {
+  // do something
+};
+
+console.log(nums[2, 2, 1]) // 1
+
+console.log(nums[4, 1, 2, 1, 2]) // 4
+
+console.log(nums[1]) // 1
