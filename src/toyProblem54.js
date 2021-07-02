03cee2369f0d503f78cccbe71f6f0e621c260d8d
21grahams <graham.kirsh@gmail.com>
2021-07-02 08:55:37 -0600

    accepted but poor run time

--- src/toyProblem54.js
+++ src/toyProblem54.js
@@ -8,18 +8,18 @@ You must implement a solution with a linear runtime complexity and use only cons
 // output: a single number which is the one number in the array that isn't duplicated
 // constraints: 1 <= nums.length <= 3 * 10^4 ||| -3 * 10^4 <= nums[i] <= 3 * 10^4 || Each element in the array appears twice except for one element which appears only once.
 // edge cases: if nums array contains only one integer, return that num
-const singleNumber = nums => {
-  // account for edge case
+const singleNumber = (nums) => {
+  let sorted = nums.sort();
 
-  // sort numbers
-
-  // loop over nums
-    // if previous value doesn't equal current value and next value doesn't equal current value
-      // return current value
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i - 1] !== sorted[i] && sorted[i + 1] !== sorted[i]) {
+      return sorted[i];
+    }
+  }
 };
 
-console.log(nums[2, 2, 1]) // 1
+console.log(nums[(2, 2, 1)]); // 1
 
-console.log(nums[4, 1, 2, 1, 2]) // 4
+console.log(nums[(4, 1, 2, 1, 2)]); // 4
 
-console.log(nums[1]) // 1
+console.log(nums[1]); // 1
