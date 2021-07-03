d9b74c75fd0c8cfe4d14c76bfc06808cce4cfba3
21grahams <graham.kirsh@gmail.com>
2021-07-03 14:51:17 -0600

    leetcode - Single Number II

---
+++ src/toyProblem55.js
@@ -0,0 +1,15 @@
+/* Leetcode - Single Number II
+
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space */
+
+const singleNumber = nums => {
+  // do something
+};
+
+console.log(singleNumber([2, 2, 3, 2])) // 3
+
+console.log(singleNumber([0, 1, 0, 1, 0, 1, 99])) // 3
+
+console.log(singleNumber([1])) // 1
