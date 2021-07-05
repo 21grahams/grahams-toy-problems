d53cd1228de603e81e5d24f9163dec340678a462
21grahams <graham.kirsh@gmail.com>
2021-07-04 20:55:01 -0600

    Runtime is 80ms and faster than 75% of JV online submissions

--- src/toyProblem55.js
+++ src/toyProblem55.js
@@ -11,8 +11,10 @@ You must implement a solution with a linear runtime complexity and use only cons
 const singleNumber = (nums) => {
   // account for edge case
   if (nums.length === 0) return nums[0];
+
   // sort the nums array
   nums = nums.sort();
+
   // loop over nums array
   for (let i = 0; i < nums.length; i++) {
     // if the element both two and one less than current element don't equal current element AND both two and one greater than current element don't equal current element
@@ -27,6 +29,9 @@ const singleNumber = (nums) => {
     }
   }
 };
+
+// Runtime: 80 ms, faster than 75.09% of JavaScript online submissions for Single Number II.
+// Memory Usage: 39.7 MB, less than 73.31% of JavaScript online submissions for Single Number II.
 
 console.log(singleNumber([2, 2, 3, 2])); // 3
 
