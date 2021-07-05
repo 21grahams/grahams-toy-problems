670f61575bfadc837c73d2ee932f163b4c5f55b4
21grahams <graham.kirsh@gmail.com>
2021-07-04 20:54:11 -0600

    solution code implemented. Passing all leetcode test cases

--- src/toyProblem55.js
+++ src/toyProblem55.js
@@ -8,18 +8,28 @@ You must implement a solution with a linear runtime complexity and use only cons
 // output: a single number which is the only non-repeating integer
 // constraints: 1 <= nums.length <= 3 * 10^4 || -2^31 <= nums[i] <= 2^31 - 1
 // edge cases: if the array contains only one number, return that number
-const singleNumber = nums => {
+const singleNumber = (nums) => {
   // account for edge case
-
+  if (nums.length === 0) return nums[0];
   // sort the nums array
-
+  nums = nums.sort();
   // loop over nums array
-    // if the element both two and one less than current element don't equal current element AND both two and more greater than current element don't equal current element
+  for (let i = 0; i < nums.length; i++) {
+    // if the element both two and one less than current element don't equal current element AND both two and one greater than current element don't equal current element
+    if (
+      nums[i - 2] !== nums[i] &&
+      nums[i - 1] !== nums[i] &&
+      nums[i + 1] !== nums[i] &&
+      nums[i + 2] !== nums[i]
+    ) {
       // return current element
+      return nums[i];
+    }
+  }
 };
 
-console.log(singleNumber([2, 2, 3, 2])) // 3
+console.log(singleNumber([2, 2, 3, 2])); // 3
 
-console.log(singleNumber([0, 1, 0, 1, 0, 1, 99])) // 3
+console.log(singleNumber([0, 1, 0, 1, 0, 1, 99])); // 3
 
-console.log(singleNumber([1])) // 1
+console.log(singleNumber([1])); // 1
