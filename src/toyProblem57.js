55f2211600a4a539b49b4fd75531ba09edebaf5d
21grahams <graham.kirsh@gmail.com>
2021-07-06 08:35:31 -0600

    solution implemented. Passing all leetcode tests

--- src/toyProblem57.js
+++ src/toyProblem57.js
@@ -2,7 +2,6 @@
 
 Given the root of a binary tree, return the preorder traversal of its nodes' values */
 
-
 // input: a binary tree of nodes
 // output: the nodes of the preorder traversal of that binary tree
 // contraints: The number of nodes in the tree is in the range [0, 100] || 100 <= Node.val <= 100
@@ -15,19 +14,33 @@ class Treenode {
   }
 }
 
-const preorderTraversal = root => {
+const preorderTraversal = (root) => {
   // account for edge case
+  if (!root) return [];
 
   // create a var to store the values of nodes visited
+  let collection = [];
+  // store the root of the tree in a var called current
+  let curr = root;
   // create helper function which accepts a node
-    // push the values of the node to the variable that stores the values
+  const helperFunc = (node) => {
+    // push the value of the node to the variable that stores the values
+    collection.push(node.val);
     // if the node has a left property
+    if (node.left) {
       // call the helper function with the left property on the node
+      helperFunc(node.left);
+    }
     // if the node has a right property
+    if (node.right) {
       // call the helper function with the left property on the node
-
+      helperFunc(node.right);
+    }
+  };
   // invoke the helper function with the current variable
+  helperFunc(curr);
   // return the array of values
+  return collection;
 };
 
 // test one
@@ -35,17 +48,16 @@ let nodeA = new Treenode(1);
 let nodeB = new Treenode(2);
 let nodeC = new Treenode(3);
 
-
 nodeA.left = null;
 nodeA.right = nodeB;
 nodeB.left = nodeC;
 nodeC.right = null;
 
-console.log(preorderTraversal(nodeA)) // [1, 2, 3]
+console.log(preorderTraversal(nodeA)); // [1, 2, 3]
 
-console.log(preorderTraversal([])) // []
+console.log(preorderTraversal([])); // []
 
-console.log(preorderTraversal([1])) // [1]
+console.log(preorderTraversal([1])); // [1]
 
 // test four
 let nodeY = new Treenode(1);
@@ -54,7 +66,7 @@ let nodeZ = new Treenode(2);
 nodeY.left = nodeZ;
 nodeA.right = null;
 
-console.log(preorderTraversal(nodeY)) // [1, 2]
+console.log(preorderTraversal(nodeY)); // [1, 2]
 
 // test five
 
@@ -63,4 +75,4 @@ let nodeK = new Treenode(2);
 
 nodeG.left = null;
 nodeG.right = nodeK;
-console.log(preorderTraversal(nodeG)) // [1, 2]
+console.log(preorderTraversal(nodeG)); // [1, 2]
