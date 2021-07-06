40e72c3eb9bfc6b72c4b280321a7bff6f382a6d9
21grahams <graham.kirsh@gmail.com>
2021-07-06 08:24:03 -0600

    IOCE implemented

--- src/toyProblem57.js
+++ src/toyProblem57.js
@@ -2,6 +2,11 @@
 
 Given the root of a binary tree, return the preorder traversal of its nodes' values */
 
+
+// input: a binary tree of nodes
+// output: the nodes of the preorder traversal of that binary tree
+// contraints: The number of nodes in the tree is in the range [0, 100] || 100 <= Node.val <= 100
+// edge cases: if tree is empty, return empty array of nodes
 class Treenode {
   constructor(val) {
     this.val = val;
