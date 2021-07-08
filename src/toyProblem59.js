41ed75509aba61936ed61d7aa837a78f1551786f
21grahams <graham.kirsh@gmail.com>
2021-07-08 08:04:17 -0600

    added test suite

--- src/toyProblem59.js
+++ src/toyProblem59.js
@@ -29,3 +29,12 @@ MinStack.prototype.top = function() {
 MinStack.prototype.getMin = function() {
 
 };
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
