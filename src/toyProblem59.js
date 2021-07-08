f7d9810ec27d9d1f7b8bb89dfb4080a1f75996d8
21grahams <graham.kirsh@gmail.com>
2021-07-08 08:02:33 -0600

    leetcode - Min Stack implemented

---
+++ src/toyProblem59.js
@@ -0,0 +1,31 @@
+/* leetcode - Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack */
+
+const MinStack = () => {
+  // do something
+};
+
+MinStack.prototype.push = function(val) {
+
+};
+
+MinStack.prototype.pop = function() {
+
+};
+
+MinStack.prototype.top = function() {
+
+};
+
+MinStack.prototype.getMin = function() {
+
+};
