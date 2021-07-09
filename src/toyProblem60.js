6299650e13843c5301d0bc06e998f086cd8943f3
21grahams <graham.kirsh@gmail.com>
2021-07-09 17:21:58 -0600

    FizzBuzz init V2

---
+++ src/toyProblem60.js
@@ -0,0 +1,6 @@
+/* FizzBuzz  * Write a function that uses the injected log function to log numbers from 1 to 100. But for multiples of 3 log "Fizz", for multiples of 5 log "Buzz", and for multiples of 15 log "FizzBuzz" */
+
+const FizzBuzz = () => {
+  // do something
+};
+
