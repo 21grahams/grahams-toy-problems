c3ece57df17fbab073fb52426d87d91629cc92a6
21grahams <graham.kirsh@gmail.com>
2021-07-09 17:32:13 -0600

    IOCE implemented

--- src/toyProblem60.js
+++ src/toyProblem60.js
@@ -1,5 +1,9 @@
 /* FizzBuzz  * Write a function that uses the injected log function to log numbers from 1 to 100. But for multiples of 3 log "Fizz", for multiples of 5 log "Buzz", and for multiples of 15 log "FizzBuzz" */
 
+// input: a function with a set of integers ranging from 1-100
+// output: set of integers from 1-100, however every multiple of 3 logs 'Fizz', every mutiple of 5 logs 'Buzz' and every multiple of 15 logs 'FizzBuzz'
+// constraints: none at this time
+// edge cases: if FizzBuzz is empty, return null
 const FizzBuzz = (string) => {
   // do something
 };
