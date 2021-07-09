ceb639cf66cf2edeb649e76213d1d96994d1aeb8
21grahams <graham.kirsh@gmail.com>
2021-07-09 17:52:21 -0600

    Pseudocode and Solution implemented

--- src/toyProblem60.js
+++ src/toyProblem60.js
@@ -4,18 +4,28 @@
 // output: set of integers from 1-100, however every multiple of 3 logs 'Fizz', every mutiple of 5 logs 'Buzz' and every multiple of 15 logs 'FizzBuzz'
 // constraints: none at this time
 // edge cases: if FizzBuzz is empty, return null
-const FizzBuzz = (string) => {
-  // do something
+const FizzBuzz = (count) => {
+  // loop over the variable, starting from count / n, up to the 100
+  for (let count = 1; count <= 100; count++) {
+    // if the count / n modulo 5 and 3 is equal to 0
+    if (count % 5 == 0 && count % 3 === 0) {
+      // use our fun typescript to log ('FizzBuzz')
+      console.log('FizzBuzz');
+      // else if the count / n modulo 3 is equal to 0
+    } else if (count % 3 === 0) {
+      // use our fun typescript to log ('Fizz')
+      console.log('Fizz');
+      // else if the count / n modulo 5 is equal to 0
+    } else if (count % 5 === 0) {
+      // use our fun typescript to log ('Buzz')
+      console.log('Buzz');
+      // else
+    } else {
+      // log i - current element
+      console.log(count)
+    }
+  }
 };
 
-console.log(FizzBuzz('3')) 'Fizz'
-
-console.log(FizzBuzz('5')) 'Buzz'
-
-console.log(FizzBuzz('4')) '4'
-
-console.log(FizzBuzz('15')) 'FizzBuzz'
-
-console.log(FizzBuzz('30')) 'FizzBuzz'
-
-console.log(FizzBuzz('31')) '31'
+let tester = FizzBuzz();
+console.log(tester);
