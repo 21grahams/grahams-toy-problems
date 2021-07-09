565140a06b2fea20a2a6ff2b540146dd368340ac
21grahams <graham.kirsh@gmail.com>
2021-07-09 17:31:39 -0600

    test suite implemented

--- src/toyProblem60.js
+++ src/toyProblem60.js
@@ -1,6 +1,17 @@
 /* FizzBuzz  * Write a function that uses the injected log function to log numbers from 1 to 100. But for multiples of 3 log "Fizz", for multiples of 5 log "Buzz", and for multiples of 15 log "FizzBuzz" */
 
-const FizzBuzz = () => {
+const FizzBuzz = (string) => {
   // do something
 };
 
+console.log(FizzBuzz('3')) 'Fizz'
+
+console.log(FizzBuzz('5')) 'Buzz'
+
+console.log(FizzBuzz('4')) '4'
+
+console.log(FizzBuzz('15')) 'FizzBuzz'
+
+console.log(FizzBuzz('30')) 'FizzBuzz'
+
+console.log(FizzBuzz('31')) '31'
