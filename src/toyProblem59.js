79a5d988712a12b70552063fcad5e9e8bd6752d5
21grahams <graham.kirsh@gmail.com>
2021-07-10 09:46:25 -0600

    IOCE implemented

--- src/toyProblem59.js
+++ src/toyProblem59.js
@@ -10,6 +10,10 @@ void pop() removes the element on the top of the stack.
 int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack */
 
+// input: an empty stack that I will be creating, initializing and building methods on including push, pop, top and retrieving
+// output: The minStack function and the methods on MinStack completed
+// constraints: -2^31 <= val <= 2^31 - 1 || Methods pop, top and getMin operations will always be called on non-empty stacks. At most 3 * 10^4 calls will be made to push, pop, top, and getMin
+// edge cases: none at this time
 const MinStack = () => {
   // do something
 };
