c0d1c6ac3128b4f375bd51d243eb73fd441f77ab
21grahams <graham.kirsh@gmail.com>
2021-07-11 09:40:18 -0600

    IOCE implemented

--- src/toyProblem61.js
+++ src/toyProblem61.js
@@ -6,6 +6,10 @@ Return the indices of the two numbers (1-indexed) as an integer array answer of 
 
 The tests are generated such that there is exactly one solution. You may not use the same element twice */
 
+// input: an array of ascending integers and a target integer
+// output: the indicies of two specific numbers that sum to target integer
+// constraints: the array of numbers, thus the return output of indicies are 1-indexed. NOT 0. The tests are generated such that there is exactly one solution
+// edge cases: none at this time
 const twoSum = (numbers, targets) => {
   // do something
 };
