1264383755e45551d7d05efe79a5fbf09de5497c
21grahams <graham.kirsh@gmail.com>
2021-07-11 09:35:03 -0600

    test suite initialized

--- src/toyProblem61.js
+++ src/toyProblem61.js
@@ -9,3 +9,10 @@ The tests are generated such that there is exactly one solution. You may not use
 const twoSum = (numbers, targets) => {
   // do something
 };
+
+console.log(twoSum([2, 7, 11, 15], 9)) // [1, 2]
+// Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+
+console.log(twoSum([2, 3, 4], 6)) // [1, 3]
+
+console.log(twoSum([-1, 0], -1)) // [1, 2]
