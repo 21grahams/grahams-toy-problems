ce23eaaba26ff8f0423f9cb2b4d56cc74a3c1dff
21grahams <graham.kirsh@gmail.com>
2021-07-12 08:20:34 -0600

    test suite implemented as well as ListNode class

--- src/toyProblem62.js
+++ src/toyProblem62.js
@@ -2,6 +2,26 @@
 
 Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null */
 
+class Listnode() {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
 const getIntersectionNode = (headA, headB) => {
   // do something
 };
+
+console.log(getIntersectionNode([4, 1, 8, 4, 5], [5, 6, 1, 8, 4, 5])) // 8
+// Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+// From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+
+console.log(getIntersectionNode([1, 9, 1, 2, 4], [3, 2, 4])) // 2
+// Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+// From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+console.log(getIntersectionNode([2, 6, 4], [1, 5])) // null
+// Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+// Explanation: The two lists do not intersect, so return null.
+
