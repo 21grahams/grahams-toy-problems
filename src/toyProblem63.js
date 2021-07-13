2736d02fcc9db0e4f02e4fa9126e09208970fe37
21grahams <graham.kirsh@gmail.com>
2021-07-13 09:33:19 -0600

    solution implemented

--- src/toyProblem63.js
+++ src/toyProblem63.js
@@ -6,21 +6,56 @@ Given an integer columnNumber, return its corresponding column title as it appea
 // output: that integers corresponding letter as it appears in an Excel sheet (A -> 1, B -> 2, etc)
 // constraints: 1 <= columnNumber <= 2^31 - 1
 // edge cases: if input is empty, return null
-const convertToTitle = columnNumber => {
-  // account for edge case
-  // create a number variable, set equal to 0
+const convertToTitle = n => {
+  let charMap = {
+    0: "Z",
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H",
+    9: "I",
+    10: "J",
+    11: "K",
+    12: "L",
+    13: "M",
+    14: "N",
+    15: "O",
+    16: "P",
+    17: "Q",
+    18: "R",
+    19: "S",
+    20: "T",
+    21: "U",
+    22: "V",
+    23: "W",
+    24: "X",
+    25: "Y"
+};
 
-  // create a object with props referring to 1-26 and A-Z respectively
 
-  // if the columnNumber is fewer than 26
-    // return the corresponding letter equevilant
-  // else
-    // create a power variable. Set equal to 0
-    // loop over columnNumber starting at columnNumber, ending at 0, decrementing
-      // assign number to the Math.pow of 26 and power and multiply by columnNumber at the current element
-      // increment the power
+let title = "";
 
-  // return the number
+if (n <= 26) {
+    return charMap[n%26]
+}
+
+while (n > 0) {
+    let r = n%26;
+
+    n = Math.floor(n/26);
+
+    if (r == 0) {
+        r = 0;
+        n = n-1;
+    }
+    title = charMap[r] + title;
+}
+
+return title;
 };
 
 console.log(convertToTitle(1)) // A
