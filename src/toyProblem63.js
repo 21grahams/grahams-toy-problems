3661286d010a326d8981755bb799e383096f5461
21grahams <graham.kirsh@gmail.com>
2021-07-13 08:30:54 -0600

    test suite implemented

--- src/toyProblem63.js
+++ src/toyProblem63.js
@@ -5,3 +5,11 @@ Given an integer columnNumber, return its corresponding column title as it appea
 const convertToTitle = columnNumber => {
   // do something
 };
+
+console.log(convertToTitle(1)) // A
+
+console.log(convertToTitle(28)) // AB
+
+console.log(convertToTitle(701)) // ZY
+
+console.log(convertToTitle(2147483647)) // FXSHRXW
