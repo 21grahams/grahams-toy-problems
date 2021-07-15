003702df76353f91c0ba915b02e6fbcc30539852
21grahams <graham.kirsh@gmail.com>
2021-07-15 12:45:42 -0600

    updated pseudocode

--- src/toyProblem64.js
+++ src/toyProblem64.js
@@ -11,10 +11,18 @@ The majority element is the element that appears more than ⌊n / 2⌋ times. Yo
 const majorityElement = nums => {
   // account for edge case
   // create a count var, start at 0
+  // create a masterObj
 
   // sort the nums input array
   // loop over the nums input array
-    // if current element is greater than half of length of nums input array
+    // if the current element is undefined
+      // set the masterObj of current element to 1
+    // otherwise
+      // increment the masterObj of current element
+
+  // loop over the masterObj using a for-in loop
+    // grab the value of each key
+    // if value is greater than half of length of nums input array
       // increment count with current element; we've found our majority number
 
   // return count
