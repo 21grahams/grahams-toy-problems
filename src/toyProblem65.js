8939ab7360a629bd4e13e71a34f19f83b19ab1f9
21grahams <graham.kirsh@gmail.com>
2021-07-16 08:23:05 -0600

    test suite implemented

--- src/toyProblem65.js
+++ src/toyProblem65.js
@@ -1,4 +1,4 @@
-/* leetcode Factorial Trailing Zeros
+/* leetcode Factorial Trailing Zeroes
 
 Given an integer n, return the number of trailing zeroes in n!.
 
@@ -7,3 +7,11 @@ Follow up: Could you write a solution that works in logarithmic time complexity?
 const trailingZeroes = n => {
   // do something
 };
+
+console.log(trailingZeroes(3)) // 0
+// Explanation: 3! = 6, no trailing zero.
+
+console.log(trailingZeroes(5)) // 1
+// Explanation: 5! = 120, one trailing zero.
+
+console.log(trailingZeroes(0)) // 0
