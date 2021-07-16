c054fa669baf71dc3492f2e391283e752b888b23
21grahams <graham.kirsh@gmail.com>
2021-07-16 08:20:50 -0600

    leetcode init Factorial Trailing Zeroes

---
+++ src/toyProblem65.js
@@ -0,0 +1,9 @@
+/* leetcode Factorial Trailing Zeros
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Follow up: Could you write a solution that works in logarithmic time complexity? */
+
+const trailingZeroes = n => {
+  // do something
+};
