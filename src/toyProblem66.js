ca8fba7511fae041b354bc54e069bdf22867f151
21grahams <graham.kirsh@gmail.com>
2021-07-19 10:12:55 -0600

    test suite implemented

--- src/toyProblem66.js
+++ src/toyProblem66.js
@@ -6,3 +6,8 @@ const containsDuplicate = nums => {
   // do something
 };
 
+console.log(containsDuplicate([1, 2, 3, 1])) // true
+
+console.log(containsDuplicate([1, 2, 3, 4])) // false
+
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])) // true
