652c86d4c6086d72d4ae5c50bf0b5f260728f3c5
21grahams <graham.kirsh@gmail.com>
2021-07-20 08:43:58 -0600

    IOCE implemented

--- src/toyProblem67.js
+++ src/toyProblem67.js
@@ -2,6 +2,10 @@
 
 Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k */
 
+// input: an array of integers (could be positive or negative) and another integer as a second param
+// output: a boolean. True if there are two distinct indices and nums[i] - nums[j] is less or equal to k
+// constraints: 1 <= nums.length <= 10^5 | -10^9 <= nums[i] <= 10^9 | 0 <= k <= 10^5
+// edge cases: if nums array is empty, return null
 const containsNearbyDuplicate = (nums, k) => {
   // do something
 };
