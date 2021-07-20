c0c7ddcf7cdb139cd5df2f0a43d55aef224296bd
21grahams <graham.kirsh@gmail.com>
2021-07-20 08:38:17 -0600

    test suite implemented

--- src/toyProblem67.js
+++ src/toyProblem67.js
@@ -5,3 +5,9 @@ Given an integer array nums and an integer k, return true if there are two disti
 const containsNearbyDuplicate = (nums, k) => {
   // do something
 };
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3)) // true
+
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1)) // true
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2)) // false
