5d91d44ed271ad7d67c43b92db48a5ee16275dac
21grahams <graham.kirsh@gmail.com>
2021-07-20 08:50:53 -0600

    initial pseudocode implemented

--- src/toyProblem67.js
+++ src/toyProblem67.js
@@ -7,7 +7,15 @@ Given an integer array nums and an integer k, return true if there are two disti
 // constraints: 1 <= nums.length <= 10^5 | -10^9 <= nums[i] <= 10^9 | 0 <= k <= 10^5
 // edge cases: if nums array is empty, return null
 const containsNearbyDuplicate = (nums, k) => {
-  // do something
+  // account for edge case
+
+  // sort the nums array
+  // loop over the input array
+    // loop over the input array a second time, iterating from first loop + 1
+      // if the current element of first loop is equal to current element of second loop AND current element of first loop minus current element of second loop is less than or equal to k
+        // return true
+
+  // return false - we've found no dupes and broken out of both loops
 };
 
 console.log(containsNearbyDuplicate([1, 2, 3, 1], 3)) // true
