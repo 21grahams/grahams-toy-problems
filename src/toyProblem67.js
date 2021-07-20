145953b538a5f4b2f6c1fe6167ab0e56d3377ffd
21grahams <graham.kirsh@gmail.com>
2021-07-20 08:36:46 -0600

    leetcode init Contains Duplicate II

---
+++ src/toyProblem67.js
@@ -0,0 +1,7 @@
+/* leetcode Contains Duplicate II
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k */
+
+const containsNearbyDuplicate = (nums, k) => {
+  // do something
+};
