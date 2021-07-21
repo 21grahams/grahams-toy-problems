c48a5fe5f4679b6fd3a2256e8c70681244c21001
21grahams <graham.kirsh@gmail.com>
2021-07-21 12:52:05 -0600

    time limit exceeded on leetcode so terrible time complexity, although I like this particular flesh out of this problem

--- src/toyProblem68.js
+++ src/toyProblem68.js
@@ -6,14 +6,20 @@ Count the number of prime numbers less than a non-negative number, n */
 // output: the number of prime numbers less than the input value of n
 // constraints: 0 <= n <= 5 * 10^6
 // edge cases:  if n doesn't exist, return null || if n equals 0 or 1, return 0
-const countPrimes = (n, count = 0, array = []) => {
-  for (let i = 2; i < n; i++) {
-    if (array[i]) continue;
-    else count++;
-    for (let j = i + i; j <= n; j += i) array[j] = true;
-  }
+const countPrimes = (n) => {
 
+  if (n == 0 || n == 1) return 0;
+  let count = 0;
+
+  for (let i = 2; i < n; i++) if (isPrime(i)) count++;
   return count;
+};
+
+const isPrime = (n) => {
+  if (n == 2) return true;
+  for (let i = 2; i <= Math.sqrt(n); i++) if (n % i == 0) return false;
+
+  return true;
 };
 
 // Runtime: 428 ms, faster than 39.49% of JavaScript online submissions for Count Primes.
