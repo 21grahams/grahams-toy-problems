2738ba0fcc0c4d5dd22e8757c1354e3ed9985df1
21grahams <graham.kirsh@gmail.com>
2021-07-21 09:46:37 -0600

    added test suite

--- src/toyProblem68.js
+++ src/toyProblem68.js
@@ -5,3 +5,10 @@ Count the number of prime numbers less than a non-negative number, n */
 const countPrimes = n => {
   // do something
 };
+
+console.log(countPrimes(10)); // 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7
+
+console.log(countPrimes(0)); // 0
+
+console.log(countPrimes(1)); // 0
