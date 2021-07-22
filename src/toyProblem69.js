c845b1c4326e3d8e52f91e0e5cd16de5665f8412
21grahams <graham.kirsh@gmail.com>
2021-07-22 11:03:46 -0600

    Initial pseudocode implemented

--- src/toyProblem69.js
+++ src/toyProblem69.js
@@ -14,7 +14,24 @@ Each range [a,b] in the list should be output as:
 // constraints: 0 <= nums.length <= 20 | -2^31 <= nums[i] <= 2^31 - 1 All the values of nums are unique | nums is sorted in ascending order.
 // edge cases: if the input array is empty, return an empty array. If input array contains one integer, return that integer
 const summaryRanges = nums => {
-  // do something
+  // account for edge cases
+
+  // create a start variable, assign to null
+  // create a result array
+
+  // loop over input array
+    // if start is equal to null
+      // assign start to current element of input array
+    // if current element of input array is equal to next element of input array minus one
+      // continue
+    // if current element of input array is equal to start
+      // push current element onto result array and convert toString
+      // assign start to null
+    // otherwise
+      // push template literal of start to current element onto result array
+      // assign start to null
+
+  // return result
 };
 
 console.log(summaryRanges([0, 1, 2, 4, 5, 7])) // ["0->2","4->5","7"]
