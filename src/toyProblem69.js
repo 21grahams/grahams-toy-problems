3c3cc033c9c85feeb0e356e218c88cb106e0da7f
21grahams <graham.kirsh@gmail.com>
2021-07-22 11:29:13 -0600

    new solution implemented and passing leetcode tests

--- src/toyProblem69.js
+++ src/toyProblem69.js
@@ -14,39 +14,17 @@ Each range [a,b] in the list should be output as:
 // constraints: 0 <= nums.length <= 20 | -2^31 <= nums[i] <= 2^31 - 1 All the values of nums are unique | nums is sorted in ascending order.
 // edge cases: if the input array is empty, return an empty array. If input array contains one integer, return that integer
 const summaryRanges = (nums) => {
-  // // create a start variable, assign to null
-  let start = null;
-  // create a result array
-  let result = [];
-
-  // loop over input array
-  for (let i = 0; i < nums.length; i++) {
-    // if start is equal to null
-    if (start === null) {
-      // assign start to current element of input array
-      start = nums[i];
-    }
-    // if current element of input array is equal to next element of input array minus one
-    if (nums[i] === nums[i + 1] - 1) {
-      // continue
-      continue;
-    }
-    // if current element of input array is equal to start
-    if (nums[i] === start) {
-      // push current element onto result array and convert toString
-      result.push(nums[i].toString());
-      // assign start to null
-      start = null;
-      // otherwise
-    } else {
-      // push template literal of start to current element onto result array
-      result.push(`${start}->${nums[i]}`);
-      // assign start to null
-      start = null;
+  let i = 0,
+    res = [];
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j + 1] === nums[j] + 1) continue;
+    else {
+      if (i == j) res.push(nums[i] + "");
+      else res.push(nums[i] + "->" + nums[j]);
+      i = j + 1;
     }
   }
-  // return result
-  return result;
+  return res;
 };
 
 // Runtime: 76 ms, faster than 53.00% of JavaScript online submissions for Summary Ranges.
