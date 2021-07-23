f54344e5735a9168d59208b7d0b8493699f04d33
21grahams <graham.kirsh@gmail.com>
2021-07-23 10:34:12 -0600

    test suite implemented

--- src/toyProblem70.js
+++ src/toyProblem70.js
@@ -7,3 +7,15 @@ Follow up: Could you implement a solution using only O(1) extra space complexity
 const missingNumber = nums => {
   // do something
 };
+
+console.log(missingNumber([3, 0, 1])) // 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+console.log(missingNumber([0, 1]) // 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])) // 8
+// Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+console.log(missingNumber([0])) // 1
+// Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
