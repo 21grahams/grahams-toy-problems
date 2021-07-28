cdc9fd9e0290ed592122057f1dbff04193084eac
21grahams <graham.kirsh@gmail.com>
2021-07-28 16:54:32 -0700

    added initial pseudocode back in after test

--- src/toyProblem72.js
+++ src/toyProblem72.js
@@ -7,7 +7,7 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 // constraints: 1 <= s.length, t.length <= 5 * 10^4 | s and t consist of lowercase English letters
 // edge cases: if s and t are empty, return true
 const isAnagram = (s, t) => {
-
+  // do something
 };
 
 console.log(isAnagram('anagram', 'nagaram')) // true
