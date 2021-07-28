08f7b36b06ef51e75803a927b21f7990d3ccb7eb
21grahams <graham.kirsh@gmail.com>
2021-07-28 10:16:55 -0700

    IOCE implemented

--- src/toyProblem72.js
+++ src/toyProblem72.js
@@ -2,6 +2,10 @@
 
 Given two strings s and t, return true if t is an anagram of s, and false otherwise */
 
+// input: two strings, s and t
+// output: a boolean, that returns true if s and t are anagrams of each other, false if not
+// constraints: 1 <= s.length, t.length <= 5 * 10^4 | s and t consist of lowercase English letters
+// edge cases: if s and t are empty, return true
 const isAnagram = (s, t) => {
   // do something
 };
