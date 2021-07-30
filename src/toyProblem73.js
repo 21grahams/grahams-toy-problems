b68d562d81957c441467f5dbb0844bc31c4f6cbe
21grahams <graham.kirsh@gmail.com>
2021-07-30 09:51:24 -0700

    test suite implemented

--- src/toyProblem73.js
+++ src/toyProblem73.js
@@ -5,3 +5,8 @@ Given two integer arrays nums1 and nums2, return an array of their intersection.
 const intersection = (nums1, nums2) => {
   // do something
 };
+
+console.log(intersection([1, 2, 2, 1], [2, 2])) // [2]
+
+console.log(intersection([4, 9, 5], 9, 4, 9, 8, 4)) // [9,4]
+// Explanation: [4,9] is also accepted
