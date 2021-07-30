c8c93f75496ea6672f72833f954b1c0c3ed6aeea
21grahams <graham.kirsh@gmail.com>
2021-07-30 09:47:58 -0700

    leetcode init - Intersection of Two Arrays

---
+++ src/toyProblem73.js
@@ -0,0 +1,7 @@
+/* leetcode - Intersection of Two Arrays
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order */
+
+const intersection = (nums1, nums2) => {
+  // do something
+};
