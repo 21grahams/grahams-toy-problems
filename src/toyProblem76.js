690158a3bf06375b3a06d73ae3359ca7682de144
21grahams <graham.kirsh@gmail.com>
2021-08-02 13:43:48 -0700

    runtime 128ms, faster than 60% of online submissions

--- src/toyProblem76.js
+++ src/toyProblem76.js
@@ -26,6 +26,9 @@ const findDisappearedNumbers = (nums) => {
   return resultArr;
 };
 
+// Runtime: 128 ms, faster than 60.33% of JavaScript online submissions for Find All Numbers Disappeared in an Array.
+// Memory Usage: 50.3 MB, less than 29.15% of JavaScript online submissions for Find All Numbers Disappeared in an Array.
+
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // [5, 6]
 
 console.log(findDisappearedNumbers([1, 1])); // [2]
