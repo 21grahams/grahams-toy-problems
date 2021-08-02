ad18b5e67430f0346cc10a857479f8a6199eb05a
21grahams <graham.kirsh@gmail.com>
2021-08-02 13:00:27 -0700

    IOCE implemented

--- src/toyProblem76.js
+++ src/toyProblem76.js
@@ -2,6 +2,10 @@
 
 Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums */
 
+// input: an array of nums of n length. nums[i] is in the range [1, n]
+// output: an array of all integers in the range [1, n] that do not appear in nums
+// constraints: n == nums.length | 1 <= n <= 105 | 1 <= nums[i] <= n
+// edge cases: if nums is empty, return empty array
 const findDisappearedNumbers = nums => {
   // do something
 };
