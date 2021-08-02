40fabccf8779fc7df651d8943c3772a62dc95cf1
21grahams <graham.kirsh@gmail.com>
2021-08-01 21:25:17 -0700

    test suite implemented

--- src/toyProblem75.js
+++ src/toyProblem75.js
@@ -5,3 +5,13 @@ Given integer array nums, return the third maximum number in this array. If the 
 const thirdMax = nums => {
   // do something
 };
+
+console.log(thirdMax([3, 2, 1])) // 1
+// Explanation: The third maximum is 1.
+
+console.log(thirdMax([1, 2])) // 2
+// Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+
+console.log(thirdMax([2, 2, 3, 1])) // 1
+// Explanation: Note that the third maximum here means the third maximum distinct number.
+// Both numbers with value 2 are both considered as second maximum.
