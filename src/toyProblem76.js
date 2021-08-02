4c1b53d228dd1780b6bce14258354c3958b6569a
21grahams <graham.kirsh@gmail.com>
2021-08-02 13:43:24 -0700

    solution implemented and passing all leetcode tests

--- src/toyProblem76.js
+++ src/toyProblem76.js
@@ -6,19 +6,26 @@ Given an array nums of n integers where nums[i] is in the range [1, n], return a
 // output: an array of all integers in the range [1, n] that do not appear in nums
 // constraints: n == nums.length | 1 <= n <= 105 | 1 <= nums[i] <= n
 // edge cases: if nums is empty, return empty array
-const findDisappearedNumbers = nums => {
+const findDisappearedNumbers = (nums) => {
   // account for edge case
+  if (nums.length === 0) return [];
   // create a resultArr
+  let resultArr = [];
   // create a new set to drop dupes
+  let newSet = new Set(nums);
   // create a max, assign to length of nums
+  let max = nums.length;
 
   // loop over new set, up to max
+  for (let i = 1; i <= max; i++) {
     // if new set doesn't have/contain i
       // push to resultArr
-
+    if (!newSet.has(i)) resultArr.push(i);
+  }
   // return resultArr
+  return resultArr;
 };
 
-console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])) // [5, 6]
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // [5, 6]
 
-console.log(findDisappearedNumbers([1, 1])) // [2]
+console.log(findDisappearedNumbers([1, 1])); // [2]
