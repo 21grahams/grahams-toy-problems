98f2a574e95239a9642a88b7bc47ca9251de984c
21grahams <graham.kirsh@gmail.com>
2021-08-01 21:22:31 -0700

    leetcode init - Third Maximum Number

---
+++ src/toyProblem75.js
@@ -0,0 +1,7 @@
+/* leetcode - Third Maximum Number
+
+Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number */
+
+const thirdMax = nums => {
+  // do something
+};
