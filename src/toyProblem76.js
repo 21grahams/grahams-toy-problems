7de6b56787f22e9f419e89a2aa423e25ab36ca42
21grahams <graham.kirsh@gmail.com>
2021-08-02 13:12:44 -0700

    initial pseudocode implemented

--- src/toyProblem76.js
+++ src/toyProblem76.js
@@ -7,7 +7,17 @@ Given an array nums of n integers where nums[i] is in the range [1, n], return a
 // constraints: n == nums.length | 1 <= n <= 105 | 1 <= nums[i] <= n
 // edge cases: if nums is empty, return empty array
 const findDisappearedNumbers = nums => {
-  // do something
+  // account for edge case
+  // create a resultArr
+
+  // sort the input array
+  // create a max, assign to length of input array
+
+  // loop over input array of to max
+    // if current element of input array is not one greater than current element minus one
+      // push current element minus one plus one to the resultArr
+
+  // return resultArr
 };
 
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])) // [5, 6]
