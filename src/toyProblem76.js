85feab689c503fd388b23c084923f79f4f616fb8
21grahams <graham.kirsh@gmail.com>
2021-08-02 11:37:06 -0700

    test suite implemented

--- src/toyProblem76.js
+++ src/toyProblem76.js
@@ -5,3 +5,7 @@ Given an array nums of n integers where nums[i] is in the range [1, n], return a
 const findDisappearedNumbers = nums => {
   // do something
 };
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])) // [5, 6]
+
+console.log(findDisappearedNumbers([1, 1])) // [2]
