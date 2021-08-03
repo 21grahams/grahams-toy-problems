7598dd6949994d4c39617e106a9d12d810b4ec09
21grahams <graham.kirsh@gmail.com>
2021-08-03 11:52:26 -0700

    test suite implemented

--- src/toyProblem77.js
+++ src/toyProblem77.js
@@ -16,3 +16,20 @@ const binaryTreePaths = root => {
   // do something
 };
 
+// test one
+let nodeA = new TreeNode(1);
+let nodeB = new TreeNode(2);
+let nodeC = new TreeNode(3);
+let nodeD = new TreeNode(5);
+
+nodeA.left = nodeB;
+nodeA.right = nodeC;
+nodeB.left = null;
+nodeB.right = nodeD;
+
+console.log(binaryTreePaths(nodeA)) // ["1->2->5", "1->3"]
+
+// test two
+let nodeG = new TreeNode(1);
+
+console.log(binaryTreePaths(nodeG)) // ["1"]
