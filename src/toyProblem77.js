d61a5573f9e636076e9af323f9f6c5f77b5af997
21grahams <graham.kirsh@gmail.com>
2021-08-03 11:47:06 -0700

    added TreeNode class

--- src/toyProblem77.js
+++ src/toyProblem77.js
@@ -4,6 +4,15 @@ Given the root of a binary tree, return all root-to-leaf paths in any order.
 
 A leaf is a node with no children */
 
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+};
+
 const binaryTreePaths = root => {
   // do something
 };
+
