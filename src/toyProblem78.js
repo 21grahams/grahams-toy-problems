45b657bc1a3fcca0ebe994b0d144aba99b4769aa
21grahams <graham.kirsh@gmail.com>
2021-08-05 09:12:27 -0700

    IOCE implemented

--- src/toyProblem78.js
+++ src/toyProblem78.js
@@ -4,6 +4,10 @@ Given two non-negative integers, num1 and num2 represented as string, return the
 
 You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly */
 
+// input: two non-negative integers, represented as strings
+// output: the sum of both integers, represented as a string
+// constraints: can't use any built-in library for handling large integers (such as BigInteger). Cannot convert the inputs to integers directly
+// edge cases: if both numbers are 0, return 0
 const addStrings = (num1, num2) => {
   // do something
 };
