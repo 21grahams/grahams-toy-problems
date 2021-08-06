3712f7e14d7076e8cbdc90253601019410171be5
21grahams <graham.kirsh@gmail.com>
2021-08-06 09:06:12 -0700

    test suite implemented

--- src/toyProblem79.js
+++ src/toyProblem79.js
@@ -7,3 +7,9 @@ In one move, you can increment n - 1 elements of the array by 1 */
 const minMoves = nums => {
   // do something
 };
+
+console.log(minMoves([1, 2, 3])) // 3
+// Explanation: Only three moves are needed (remember each move increments two elements):
+// [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+
+console.log(minMoves([1, 1, 1])) // 0
