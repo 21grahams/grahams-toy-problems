fa25b17dc3a6a3d96d59dd6b96c71b46e67c5262
21grahams <graham.kirsh@gmail.com>
2021-08-08 15:03:12 -0700

    test suite implemented

--- src/toyProblem80.js
+++ src/toyProblem80.js
@@ -7,3 +7,12 @@ Each child i has a greed factor g[i], which is the minimum size of a cookie that
 const findContentChildren = (g, s) => {
   // do something
 };
+
+console.log(findContentChildren([1, 2, 3], [1, 1])) // 1
+/* Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3.
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content. You need to output 1 */
+
+console.log(findContentChildren([1, 2], [1, 2, 3])) // 2
+/* Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2.
+You have 3 cookies and their sizes are big enough to gratify all of the children,
+You need to output 2 */
