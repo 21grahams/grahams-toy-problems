230a5b4d0524520f41822ba7b570e3e2f75bad63
21grahams <graham.kirsh@gmail.com>
2021-08-10 17:49:04 -0600

    IOCE implemented

--- src/toyProblem81.js
+++ src/toyProblem81.js
@@ -2,6 +2,10 @@
 
 Given a binary array nums, return the maximum number of consecutive 1's in the array */
 
+// input: an array of integers, appearing to all be positive
+// output: a single number, which represents the max amount of consecutive 1's in the array
+// constraints: 1 <= nums.length <= 10^5. nums[i] is either 0 or 1.
+// edge cases: if input array is empty, return 0
 const findMaxConsecutiveOnes = nums => {
   // do something
 };
