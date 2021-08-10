58c7f14b950798f93adeaf197b2e676305a2b7fe
21grahams <graham.kirsh@gmail.com>
2021-08-09 23:19:50 -0600

    test suite implemented

--- src/toyProblem81.js
+++ src/toyProblem81.js
@@ -5,3 +5,9 @@ Given a binary array nums, return the maximum number of consecutive 1's in the a
 const findMaxConsecutiveOnes = nums => {
   // do something
 };
+
+
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])) // 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])) // 2
