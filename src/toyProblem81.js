95aed4f29a04699e38ffa9e2cd3686f2d50c33ce
21grahams <graham.kirsh@gmail.com>
2021-08-09 22:18:19 -0700

    leetcode init - Max Consecutive Ones

---
+++ src/toyProblem81.js
@@ -0,0 +1,7 @@
+/* leetcode - Max Consecutive Ones
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array */
+
+const findMaxConsecutiveOnes = nums => {
+  // do something
+};
