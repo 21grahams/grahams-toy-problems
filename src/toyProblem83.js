42020602eb42f3d4fc157a0b4522cbf7d9bfeb66
21grahams <graham.kirsh@gmail.com>
2021-08-12 11:28:10 -0600

    updated pseudocode v2

--- src/toyProblem83.js
+++ src/toyProblem83.js
@@ -11,16 +11,22 @@ You must write an algorithm that runs in O(n) time and without using the divisio
 // constraints: you must write an algorithm that runs in O(n) time and without using the division operation
 // edge cases: none at this time
 const productExceptSelf = nums => {
-  // create an answer array
-  // create a result var, initialize to 0
-  // create an resultObj and assign to nums
+  // create a leftArr, assign to a new Array of the length of nums
+  // create a rightArr, assign to a new Array of the length of nums
+  // create a result array
 
-  // loop over the resultObj
-    // delete the current value from the equation
-    // calculate the product of each value, assign to result var
-    // push to answer array
+  // assign leftArr at index 0 to 1
+  // loop over nums, starting at index 1
+    // assign leftArr at current index to leftArr at index minus 1 times nums at the index minus one
 
-  // return answer array
+  // assign rightArr at the last index to 1
+  // loop over nums, decrementing and begin at nums.length - 2
+    // assign rightArr at current index to rightArr at index plus 1 times nums at the index plus one
+
+  // loop over nums
+    // push current value of leftArr times current value of rightArr onto the result array
+
+  // return result array
 };
 
 console.log(productExceptSelf([1, 2, 3, 4])) // [24, 12, 8, 6]
