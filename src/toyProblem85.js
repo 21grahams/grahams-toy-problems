759ab335203627f4c3d43898ef28995fc831910b
21grahams <graham.kirsh@gmail.com>
2021-08-15 22:15:37 -0600

    IOCE implemented

--- src/toyProblem85.js
+++ src/toyProblem85.js
@@ -8,6 +8,10 @@ Given the array nums after the rotation and an integer target, return the index 
 
 You must write an algorithm with O(log n) runtime complexity */
 
+// input: an array of integers, could be positive or negative and a target integer
+// output: the index of the array in which the target number is located
+// constraints: 1 <= nums.length <= 5000. -10^4 <= nums[i] <= 10^4. All values of nums are unique. nums is guaranteed to be rotated at some pivot. -10^4 <= target <= 10^4
+// edge cases: if the target number doesn't exist in the input, return -1
 const search = (nums, target) => {
   // do something
 };
