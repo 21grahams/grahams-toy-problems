5c3b3a6eb355099a261f5ed6b95c08cafac52b7c
21grahams <graham.kirsh@gmail.com>
2021-08-16 08:20:33 -0600

    IOCE implemented

--- src/toyProblem86.js
+++ src/toyProblem86.js
@@ -4,6 +4,10 @@ Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 
 Notice that the solution set must not contain duplicate triplets */
 
+// input: an array of integers
+// output: all the triplets of values [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// constraints: 0 <= nums.length <= 3000. -10^5 <= nums[i] <= 10^5
+// edge cases: if nums input array is empty, return empty array
 const threeSum = (nums) => {
   // do something
 };
