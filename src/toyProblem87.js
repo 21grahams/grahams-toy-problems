2670bb24182770d002227f2472a54aecfbb9ce79
21grahams <graham.kirsh@gmail.com>
2021-08-17 09:40:49 -0600

    leetcode init - Sum of Two Integers

---
+++ src/toyProblem87.js
@@ -0,0 +1,7 @@
+/* leetcode - Sum of Two Integers
+
+Given two integers a and b, return the sum of the two integers without using the operators + and - */
+
+const getSum = (a, b) => {
+  // do something
+};
