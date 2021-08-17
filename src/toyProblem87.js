d3473cfbbd69371d280e17405dbf139d74876297
21grahams <graham.kirsh@gmail.com>
2021-08-17 09:52:55 -0600

    IOCE implemented

--- src/toyProblem87.js
+++ src/toyProblem87.js
@@ -2,6 +2,10 @@
 
 Given two integers a and b, return the sum of the two integers without using the operators + and - */
 
+// input: a set of two different integers
+// output: a single integer which is the sum of both input integers
+// constraints: cannot use the + or - operator. -1000 <= a, b <= 1000
+// edge cases: if a and b both equal 0, return 0
 const getSum = (a, b) => {
   // do something
 };
