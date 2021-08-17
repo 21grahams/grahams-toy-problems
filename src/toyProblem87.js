f9d29759637afb940df40e862dbabd0ecc421fd1
21grahams <graham.kirsh@gmail.com>
2021-08-17 10:01:13 -0600

    initial pseudocode implemented

--- src/toyProblem87.js
+++ src/toyProblem87.js
@@ -7,7 +7,14 @@ Given two integers a and b, return the sum of the two integers without using the
 // constraints: cannot use the + or - operator. -1000 <= a, b <= 1000
 // edge cases: if a and b both equal 0, return 0
 const getSum = (a, b) => {
-  // do something
+  // account for edge case
+
+  // while b is not 0
+    // create a carry variable, set equal to a and b
+    // assign a to a to the power of b
+    // assign b to carry smaller than 1
+
+  // return a
 };
 
 
