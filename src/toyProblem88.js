7931123a3db826551fe2ae03bfa86134f0c0df4a
21grahams <graham.kirsh@gmail.com>
2021-08-19 09:33:28 -0600

    IOCE implemented

--- src/toyProblem88.js
+++ src/toyProblem88.js
@@ -6,6 +6,10 @@ Return the fewest number of coins that you need to make up that amount. If that 
 
 You may assume that you have an infinite number of each kind of coin */
 
+// input: a set amount of coins representing an array and an amount represented as an integer
+// output: a single number representing the fewest number of coins that you need to make up the amount of the second input
+// constraints: You may assume that you have an infinite number of each kind of coin. 1 <= coins.length <= 12. 1 <= coins[i] <= 2^31 - 1. 0 <= amount <= 10^4
+// edge cases: if that amount of money cannot be made up by any combination of the coins, return -1
 const coinChange = (coins, amount) => {
   // do something
 };
