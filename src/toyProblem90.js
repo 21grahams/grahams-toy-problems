a4638da11faa8e11ee2325dc5c767a3d9c1491d0
21grahams <graham.kirsh@gmail.com>
2021-08-20 09:43:03 -0600

    test suite implemented

--- src/toyProblem90.js
+++ src/toyProblem90.js
@@ -20,6 +20,49 @@ An adjacency list is a collection of unordered lists used to represent a finite 
 
 The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph */
 
+class Node {
+  constructor(val, neighbors) {
+    this.val = val;
+    this.neighbors = neighbors;
+  }
+}
+
 const cloneGraph = node => {
-  // do something
+  return node;
 };
+
+// test one
+let nodeA = new Node(1);
+let nodeB = new Node(2);
+let nodeC = new Node(3);
+let nodeD = new Node(4);
+
+nodeA.neighbors = nodeB;
+nodeA.neighbors = nodeD;
+nodeB.neighbors = nodeA;
+nodeB.neighbors = nodeC;
+nodeC.neighbors = nodeB;
+nodeC.neighbors = nodeD;
+nodeD.neighbors = nodeA;
+nodeD.neighbors = nodeC;
+
+console.log(cloneGraph([[2,4],[1,3],[2,4],[1,3]])); // [[2,4],[1,3],[2,4],[1,3]]
+
+// test two
+let nodeE = new Node();
+
+nodeE.neighbors = null;
+
+console.log(cloneGraph([[]])) // [];
+
+// test three
+console.log(cloneGraph([])) // [];
+
+// test four
+let nodeG = new Node(1);
+let nodeK = new Node(2);
+
+nodeG.neighbors = nodeK;
+nodeK.neighbors = nodeG;
+
+console.log(cloneGraph([[2], [1]])) // [[2], [1]];
