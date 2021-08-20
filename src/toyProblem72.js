15d220e29bdddb78e16e76b5e5441fd33817e704
21grahams <graham.kirsh@gmail.com>
2021-08-20 09:24:27 -0600

    leetcode init - Clone Graph

--- src/toyProblem72.js
+++ src/toyProblem72.js
@@ -7,11 +7,14 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 // constraints: 1 <= s.length, t.length <= 5 * 10^4 | s and t consist of lowercase English letters
 // edge cases: if s and t are empty, return true
 const isAnagram = (s, t) => {
-  const map = {};
-  for (let a of s) map[a] = (map[a] || 0) + 1;
-  for (let a of t) map[a] = (map[a] || 0) - 1;
-  for (let a in map) if (map[a] !== 0) return false;
-  return true;
+  // const map = {};
+  // for (let a of s) map[a] = (map[a] || 0) + 1;
+  // for (let a of t) map[a] = (map[a] || 0) - 1;
+  // for (let a in map) if (map[a] !== 0) return false;
+  // return true;
+  s = s.split('').sort().join('');
+  t = t.split('').sort().join('');
+  return s === t;
 };
 
 // Runtime: 76 ms, faster than 98.45% of JavaScript online submissions for Valid Anagram.
