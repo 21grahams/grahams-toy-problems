f2d31c0ce90306377e1db5a5e61e0315fa84fc71
21grahams <graham.kirsh@gmail.com>
2021-08-21 13:16:36 -0600

    IOCE implemented

--- src/toyProblem91.js
+++ src/toyProblem91.js
@@ -2,6 +2,10 @@
 
 Given an array of integers and a target value, return the maximum number of subarrays that sum to equal the target number */
 
+// input: an array of positive integers and a target value represented as a positive integer
+// output: a single integer, which represents the amount of subarrays that sum to the target value
+// constraints: all integers will be positive
+// edge cases: if there are no numbers in the input array, return 0. If there is no target number, return null
 const subArrays = (nums, target) => {
   // do something
 };
