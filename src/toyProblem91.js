7244d3022d381cf561e5247bc95c677aad5eb104
21grahams <graham.kirsh@gmail.com>
2021-08-21 13:43:20 -0600

    initial pseudocode implemented

--- src/toyProblem91.js
+++ src/toyProblem91.js
@@ -7,11 +7,31 @@ Given an array of integers and a target value, return the maximum number of suba
 // constraints: all integers will be positive
 // edge cases: if there are no numbers in the input array, return 0. If there is no target number, return null
 const subArrays = (nums, target) => {
-  // do something
+  // account for edge cases
+  // create a sum variable, assign to 0
+  // create a result, assign to 0
+
+  // loop over array to use as a pointer
+    // reset sum
+    // loop over array a second time starting at i index
+      // if sum plus current element equals 100
+        // increment result
+      // otherwise
+        // increment sum and current element
+
+  // return result
 };
 
 console.log(subArray([1, 5, 15, 25, 25, 50, 100], 100)) // 2
+// explanation: [ [25, 25, 50], [100] ]
 
 console.log(subArray([], 100)) // 0
 
 console.log(subArray([1, 5, 15, 25, 25, 50, 100], 0)) // null
+
+sum = 50
+result = 1
+nums = [1, 5, 15, 25, 25, 50, 10]
+target = 100
+i = 3
+j = 5
