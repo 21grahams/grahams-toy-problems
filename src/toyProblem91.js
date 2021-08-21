d1753744243da99e1f2047fbcc3f4959605273d7
21grahams <graham.kirsh@gmail.com>
2021-08-21 13:14:24 -0600

    test suite implemented

--- src/toyProblem91.js
+++ src/toyProblem91.js
@@ -5,3 +5,9 @@ Given an array of integers and a target value, return the maximum number of suba
 const subArrays = (nums, target) => {
   // do something
 };
+
+console.log(subArray([1, 5, 15, 25, 25, 50, 100], 100)) // 2
+
+console.log(subArray([], 100)) // 0
+
+console.log(subArray([1, 5, 15, 25, 25, 50, 100], 0)) // null
