68be7be94f2f6ffc8ed9e038b0c4e60d581bfdf7
21grahams <graham.kirsh@gmail.com>
2021-08-23 09:13:51 -0600

    test suite implemented

--- src/toyProblem92.js
+++ src/toyProblem92.js
@@ -8,3 +8,9 @@ Return true if you can finish all courses. Otherwise, return false */
 const canFinish = (numCourses, prerequisites) => {
   // do something
 };
+
+console.log(canFinish(2, [[1, 0]])) // true
+// Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
+
+console.log(canFinish(2, [[1, 0], [0, 1]])) // false
+// Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
