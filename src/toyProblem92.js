91f50f657f9a69b03990018ee83f637eeb813c42
21grahams <graham.kirsh@gmail.com>
2021-08-23 09:45:09 -0600

    pseudocode and solution implemented. Passing test suite here but not everything on leetcode. Need to refactor

--- src/toyProblem92.js
+++ src/toyProblem92.js
@@ -10,11 +10,37 @@ Return true if you can finish all courses. Otherwise, return false */
 // constraints: 1 <= numCourses <= 10^5. 0 <= prerequisites.length <= 5000. prerequisites[i].length == 2. 0 <= ai, bi < numCourses. All the pairs prerequisites[i] are unique.
 // edge cases: if there are no courses to take, return false
 const canFinish = (numCourses, prerequisites) => {
-  // do something
+  // create a new map and pass in prereqs
+  const map = new Map(prerequisites);
+
+  // for of loop over prereqs
+  for (const [key, value] of prerequisites) {
+    let pre = value;
+    let arr = [key];
+    // while pre exists or equals 0
+    while (pre || pre === 0) {
+      // if arr includes pre
+      if (arr.includes(pre)) {
+        // return false
+        return false;
+      }
+      // push pre onto arr
+      arr.push(pre);
+      // assign the value of map.get to pre
+      pre = map.get(pre);
+    }
+  }
+  // return true
+  return true;
 };
 
-console.log(canFinish(2, [[1, 0]])) // true
+console.log(canFinish(2, [[1, 0]])); // true
 // Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
 
-console.log(canFinish(2, [[1, 0], [0, 1]])) // false
+console.log(
+  canFinish(2, [
+    [1, 0],
+    [0, 1],
+  ])
+); // false
 // Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
