e5e7da662262fd862d3e62d51052180d3c04b26e
21grahams <graham.kirsh@gmail.com>
2021-08-23 12:37:51 -0600

    pseudocode and solution implemented and passing all leetcode problems. Used OOP as well to incorporate test suite with nodes

--- src/toyProblem93.js
+++ src/toyProblem93.js
@@ -7,18 +7,62 @@ Given the head of a singly linked list, reverse the list, and return the reverse
 // constraints: The number of nodes in the list is the range [0, 5000]. -5000 <= Node.val <= 5000
 // edge cases: if there is no linkedlist, return an empty array
 
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-const reverseList = head => {
-  // do something
+var Node = function (val, next) {
+  this.val = val;
+  this.next = null;
 };
 
-console.log('reverseList: ', reverseList([1, 2, 3, 4, 5])) // [5, 4, 3, 2, 1]
+var reverseList = function (head) {
+  // create a previous var, assign to null
+  let prev = null;
+  // create a cur var, assign to head
+  let cur = head;
 
-console.log('reverseList: ', reverseList([1, 2])) // [2, 1]
+  // while cur exists
+  while (cur) {
+    // create next var, assign to cur.next
+    let next = cur.next;
 
-console.log('reverseList: ', reverseList([])) // []
+    // assign cur.next to be prev
+    cur.next = prev;
+    // assign prev to be cur
+    prev = cur;
+    // assign cur to be next
+    cur = next;
+  }
+  // return prev
+  return prev;
+};
+
+// test one
+let nodeA = new Node(1)
+let nodeB = new Node(2)
+let nodeC = new Node(3)
+let nodeD = new Node(4)
+let nodeE = new Node(5)
+
+nodeA.next = nodeB;
+nodeB.next = nodeC;
+nodeC.next = nodeD;
+nodeD.next = nodeE;
+nodeE.next = null;
+
+console.log("reverseList A: ", reverseList(nodeA)); // [5, 4, 3, 2, 1]
+
+
+// test two
+let nodeF = new Node(1);
+let nodeG = new Node(2);
+
+nodeF.next = nodeG;
+nodeG.next = null;
+
+console.log("reverseList: ", reverseList(nodeF)); // [2, 1]
+
+
+// test three
+let nodeX = new Node();
+
+nodeX.next = null;
+
+console.log("reverseList: ", reverseList(nodeX)); // []
