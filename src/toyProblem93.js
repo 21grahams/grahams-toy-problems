23241e9ba5500775f6df8fc24c34382bd6194b16
21grahams <graham.kirsh@gmail.com>
2021-08-23 11:00:11 -0600

    test suite implemented

--- src/toyProblem93.js
+++ src/toyProblem93.js
@@ -5,3 +5,9 @@ Given the head of a singly linked list, reverse the list, and return the reverse
 const reverseList = head => {
   // do something
 };
+
+console.log('reverseList: ', reverseList([1, 2, 3, 4, 5])) // [5, 4, 3, 2, 1]
+
+console.log('reverseList: ', reverseList([1, 2])) // [2, 1]
+
+console.log('reverseList: ', reverseList([])) // []
