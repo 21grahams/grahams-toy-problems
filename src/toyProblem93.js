61aa3c2f15c4a0f04603dc4043577b67b764959a
21grahams <graham.kirsh@gmail.com>
2021-08-23 10:58:28 -0600

    leetcode init - Reverse Linked List

---
+++ src/toyProblem93.js
@@ -0,0 +1,7 @@
+/* leetcode - Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list */
+
+const reverseList = head => {
+  // do something
+};
