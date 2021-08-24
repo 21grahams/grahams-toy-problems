2e7f2c37f2a41519701d4c81b0670a8cd9f9a1f5
21grahams <graham.kirsh@gmail.com>
2021-08-24 11:22:31 -0600

    leetcode init - Remove Nth Node From End of List

---
+++ src/toyProblem94.js
@@ -0,0 +1,7 @@
+/* leetcode - Remove Nth Node From End of List
+
+Given the head of a linked list, remove the nth node from the end of the list and return its head */
+
+const removeNthFromEnd = (head, n) => {
+  // do something
+};
