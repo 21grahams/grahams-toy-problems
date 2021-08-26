538db3bdd6ce83eb4bb18f5b5255d114a5581aa4
21grahams <graham.kirsh@gmail.com>
2021-08-26 09:36:11 -0600

    initial pseudocode implemented

--- src/toyProblem96.js
+++ src/toyProblem96.js
@@ -9,7 +9,17 @@ You must do it in place */
 // constraints: must return the matrix in place. m == matrix.length. n == matrix[0].length. 1 <= m, n <= 200. -2^31 <= matrix[i][j] <= 2^31 - 1
 // edge cases: none at this time
 const setZeroes = matrix => {
-  // do something
+  // create a var for rows, assign to length of matrix
+  // create a var for cols, assign to rows[0]
+  // create a var for rowIndex, assign to a new Array, pass in rows and fill -1
+  // create a var for colIndex, assign to a new Array, pass in cols and fill -1
+
+  // traverse all elements of input matrix
+    // if 0 is found, updated the indexes of rowIndex and colIndex to 0
+
+
+  // traverse the matrix again
+    // if there is 0 in either rowIndex or colIndex matrix and if data in origin input matrix is not 0, then change to 0
 };
 
 
