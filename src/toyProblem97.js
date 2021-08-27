73715e22b08e6f3bbcb98b86b4b49f471349977e
21grahams <graham.kirsh@gmail.com>
2021-08-27 10:47:05 -0600

    runtime 113ms, faster than 5% of online submissions. Need to refactor

--- src/toyProblem97.js
+++ src/toyProblem97.js
@@ -55,6 +55,9 @@ const spiralOrder = matrix => {
   return resultArr;
 };
 
+// Runtime: 113 ms, faster than 5.57% of JavaScript online submissions for Spiral Matrix.
+// Memory Usage: 38.5 MB, less than 85.05% of JavaScript online submissions for Spiral Matrix.
+
 console.log(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]])) // [1, 2, 3, 6, 9, 8, 7, 4, 5]
 
 console.log(spiralOrder([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])) // [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
