719e18822cbf882bf81044b76dd9afdc64a36b87
21grahams <graham.kirsh@gmail.com>
2021-08-27 09:39:32 -0600

    initial brute force pseudocode implemented

--- src/toyProblem97.js
+++ src/toyProblem97.js
@@ -7,7 +7,28 @@ Given an m x n matrix, return all elements of the matrix in spiral order. */
 // constraints: m == matrix.length. n == matrix[i].length. 1 <= m, n <= 10. -100 <= matrix[i][j] <= 100
 // edge cases: none at this time
 const spiralOrder = matrix => {
-  // do something
+  // create a resultArr
+  // create a row var, assign to length of matrix
+  // create a cols var, assign to length of matrix at index 0
+
+  // loop over rows
+    // push all values to resultArr
+    // increment rows
+    // loop over cols
+      // push last value to resultArr
+      // if this is the last row
+        // loop over rows decrementing
+          // push values to resultArr
+          // decrement cols
+          // push values at current level to resultArr
+      // otherwise
+        // increment cols
+          // loop over rows decrementing
+            // push values to resultArr
+              // decrement cols
+              // push values at current level to resultArr
+
+  // return resultArr
 };
 
 console.log(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]])) // [1, 2, 3, 6, 9, 8, 7, 4, 5]
