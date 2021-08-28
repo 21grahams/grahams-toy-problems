452151298d5e04ba943adf7a23a67a4f52bd2fc2
21grahams <graham.kirsh@gmail.com>
2021-08-28 11:10:35 -0600

    runtime 56ms, faster than 99% of online submissions

--- src/toyProblem98.js
+++ src/toyProblem98.js
@@ -28,6 +28,9 @@ const rotate = matrix => {
   return matrix;
 };
 
+// Runtime: 56 ms, faster than 99.77% of JavaScript online submissions for Rotate Image.
+// Memory Usage: 39.2 MB, less than 31.59% of JavaScript online submissions for Rotate Image.
+
 console.log(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]])) // [ [7, 4, 1], [8, 5, 2], [9, 6, 3] ]
 
 console.log(rotate([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]])) // [ [15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11] ]
