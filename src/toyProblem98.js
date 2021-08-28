1a4e425bc3f0989d17c7e66a79d4df573adff973
21grahams <graham.kirsh@gmail.com>
2021-08-28 09:56:02 -0600

    test suite implemented

--- src/toyProblem98.js
+++ src/toyProblem98.js
@@ -7,3 +7,11 @@ You have to rotate the image in-place, which means you have to modify the input 
 const rotate = matrix => {
   // do something
 };
+
+console.log(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]])) // [ [7, 4, 1], [8, 5, 2], [9, 6, 3] ]
+
+console.log(rotate([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]])) // [ [15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11] ]
+
+console.log(rotate([[1]])) // [ [1] ]
+
+console.log(rotate([[1, 2], [3, 4]])) // [ [3, 1], [4, 2] ]
