014739d03642acb778cec3afa9a81e0d2b3db663
21grahams <graham.kirsh@gmail.com>
2021-08-30 20:02:13 -0600

    IOCE implemented

--- src/toyProblem99.js
+++ src/toyProblem99.js
@@ -1,5 +1,10 @@
 /* given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A */
 
+
+// input: an array of integers. Could be positive or negative
+// output: a single integer, which represents the smallest positive integer that doesn't occur in input array A
+// constraints: none at this time
+// edge cases: none at this time
 const solution = A => {
   // do something
 };
