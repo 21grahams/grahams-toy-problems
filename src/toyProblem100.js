c2d840a396cd0dbf1a16daa7cfe749351060f533
21grahams <graham.kirsh@gmail.com>
2021-08-31 10:28:41 -0600

    IOCE implemented

--- src/toyProblem100.js
+++ src/toyProblem100.js
@@ -1,5 +1,9 @@
 /* Write a function solution that, given an integer N, returns the maximum possible value obtainable by deleting one '5' digit from the decimal representation of N. It is guaranteed that N wil contain at least one '5' digit */
 
+// input: a set of integers, could be positive or negative
+// output: the same set of integers, with the removal of one '5', which returns the largest possible set of integers
+// constraints: N contains at least one '5' always. N consists of at least two digits in decimal representation
+// edge cases: none at this time
 const solution = N => {
   // do something
 };
