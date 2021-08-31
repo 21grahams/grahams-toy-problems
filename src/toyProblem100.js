5ad572aee4d112c77e1a63047878d3ae3651f186
21grahams <graham.kirsh@gmail.com>
2021-08-31 10:25:33 -0600

    toyProblem100 init - Max Possible Value by removing one '5' digit from N

---
+++ src/toyProblem100.js
@@ -0,0 +1,5 @@
+/* Write a function solution that, given an integer N, returns the maximum possible value obtainable by deleting one '5' digit from the decimal representation of N. It is guaranteed that N wil contain at least one '5' digit */
+
+const solution = N => {
+  // do something
+};
