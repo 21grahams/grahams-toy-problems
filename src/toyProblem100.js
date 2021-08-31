2f0aa7fafbbedfd92dfc430bc927c320e0ba92bb
21grahams <graham.kirsh@gmail.com>
2021-08-31 10:34:21 -0600

    initial pseudocode implemented

--- src/toyProblem100.js
+++ src/toyProblem100.js
@@ -5,7 +5,32 @@
 // constraints: N contains at least one '5' always. N consists of at least two digits in decimal representation
 // edge cases: none at this time
 const solution = N => {
-  // do something
+  // convert N to string
+  // split string into an array of characters
+  // create a resultArr1
+  // create a resultArr2
+  // create a seen bool, assign to false
+
+  // loop over split
+    // if current element is not 5
+      // push to resultArr1
+
+    // if current element equals 5 and seen is false
+      // push to resultArr1
+      // flip seen to true
+
+  // flip seen to false
+  // loop over split a second time
+    // if current element is not 5
+      // push to resultArr2
+
+    // if current element is 5 and seen is true
+      // push to resultArr2
+
+    // if current element is 5
+      // flip seen to true
+
+  // return the highest value between resultArr1 and resultArr2
 };
 
 console.log(solution(15958)); // 1958
