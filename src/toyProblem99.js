125e182b50c1097d9dbaa67d5e7928855eb3a76a
21grahams <graham.kirsh@gmail.com>
2021-08-30 20:05:03 -0600

    solution implemented and passing all leetcode tests

--- src/toyProblem99.js
+++ src/toyProblem99.js
@@ -7,13 +7,19 @@
 // edge cases: none at this time
 const solution = A => {
   // create a sort to sort through only positive values
+  A = A.filter(x => x >= 1).sort((a, b) => a - b)
   // create a var x, assign to 1
+  let x = 1;
 
   // loop over A
+  for (let i = 0; i < A.length; i++) {
     // if we find a smaller number, no need to continue, cause array is sorted, return x
     // assign x to current element at current index of A plus 1
-
+    if (x < A[i]) return x;
+    x = A[i] + 1;
+  }
   // return x
+  return x;
 };
 
 console.log(solution([1, 3, 6, 4, 1, 2])) // 5
