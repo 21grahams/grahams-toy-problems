baa88374ac427ab33786041a774d1f63274df4ce
21grahams <graham.kirsh@gmail.com>
2021-08-31 10:55:28 -0600

    brute force solution implemented and passing all tests in test suite

--- src/toyProblem100.js
+++ src/toyProblem100.js
@@ -4,37 +4,57 @@
 // output: the same set of integers, with the removal of one '5', which returns the largest possible set of integers
 // constraints: N contains at least one '5' always. N consists of at least two digits in decimal representation
 // edge cases: none at this time
-const solution = N => {
+const solution = (N) => {
   // convert N to string
+  let string = N.toString();
   // split string into an array of characters
+  let split = string.split("");
   // create a resultArr1
+  let resultArr1 = [];
   // create a resultArr2
+  let resultArr2 = [];
   // create a seen bool, assign to false
+  let seen = false;
 
   // loop over split
+  for (let i = 0; i < split.length; i++) {
     // if current element is not 5
-      // push to resultArr1
-
+    // push to resultArr1
+    if (split[i] !== "5") resultArr1.push(split[i]);
     // if current element equals 5 and seen is false
+    if (split[i] === "5" && seen === false) {
       // push to resultArr1
+      resultArr1.push(split[i]);
       // flip seen to true
-
+      seen = true;
+    }
+  }
   // flip seen to false
+  seen = false;
   // loop over split a second time
+  for (let i = 0; i < split.length; i++) {
     // if current element is not 5
-      // push to resultArr2
-
+    // push to resultArr2
+    if (split[i] !== "5") resultArr2.push(split[i]);
     // if current element is 5 and seen is true
-      // push to resultArr2
-
+    // push to resultArr2
+    if (split[i] === "5" && seen) resultArr2.push(split[i]);
     // if current element is 5
-      // flip seen to true
-
-  // return the highest value between resultArr1 and resultArr2
+    // flip seen to true
+    if (split[i] === "5") seen = true;
+    // return the highest value between resultArr1 and resultArr2
+  }
+  let final =
+    Number(resultArr1.join("")) > Number(resultArr2.join(""))
+      ? Number(resultArr1.join(""))
+      : Number(resultArr2.join(""));
+  if (final === -0) {
+    return 0;
+  } else {
+    return final;
+  }
 };
 
 console.log(solution(15958)); // 1958
-
 console.log(solution(-5859)); // -589
-
 console.log(solution(-5000)); // 0
