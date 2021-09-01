9816a0b10f0ba6055851ff5610a60acca468d80b
21grahams <graham.kirsh@gmail.com>
2021-09-01 08:01:05 -0600

    toyProblem101 init - dice

---
+++ src/toyProblem101.js
@@ -0,0 +1,7 @@
+/* There are two sets of traditional six-faced dice, represented by two arrays A and B consisting of N and M integers respectively. Each array element has a value between 1 and 6, inclusive, representing the value of the corresponding die
+
+Write a function that given two arrays A and B, returns the minimum number of dice to be turned in order to make the sums of dice in both sets equal, or -1 if that is not possible */
+
+const dice = (a, b) => {
+  // do something
+};
