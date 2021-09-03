2a3910b092ec336e309c8894c0b19a8b9223ac53
21grahams <graham.kirsh@gmail.com>
2021-09-03 11:32:46 -0600

    solution implemented and passing all leetcode tests

--- src/toyProblem102.js
+++ src/toyProblem102.js
@@ -9,21 +9,35 @@ The word can be constructed from letters of sequentially adjacent cells, where a
 // constraints: m == board.length. n = board[i].length. 1 <= m, n <= 6. 1 <= word.length <= 15. board and word consists of only lowercase and uppercase English letters.
 // edge cases: if board is empty, return false
 const exist = (board, word) => {
-  // loop over rows of board
-    // loop over cols of board
-      // if current board square equals first letter of word AND we have found a DFS path of our word
-        // return true
-
-  // if we haven't found our path, it doesn't exist, return false
-
-  // create the DFS helper function, takes in the board, i, j, word and set count to -
+  // create the DFS helper function, takes in the board, i, j, word and set count to 0
+  let dfs = (board, i, j, word, count = 0) => {
     // if count equals the length of the word, return true (edge case)
+    if (count === word.length) return true;
     // ensure we don't go outside the boundaries of our grid. If so, return false
+    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] !== word[count]) return false;
     // create a temp var, assign to current square of board
+    let temp = board[i][j];
     // assign current square of board to empty string
+    board[i][j] = '';
     // create a found var, assign to every possible DFS search
+    let found = dfs(board, i - 1, j, word, count + 1) || dfs(board, i + 1, j, word, count + 1) || dfs(board, i, j - 1, word, count + 1) || dfs(board, i, j + 1, word, count + 1);
     // assign current square of board to temp
+    board[i][j] = temp;
     // return found
+    return found;
+  }
+
+  // loop over rows of board
+  for (let i = 0; i < board.length; i++) {
+    // loop over cols of board
+    for (let j = 0; j < board[0].length; j++) {
+      // if current board square equals first letter of word AND we have found a DFS path of our word
+      // return true
+      if (board[i][j] === word[0] && dfs(board, i, j, word)) return true;
+    }
+  }
+  // if we haven't found our path, it doesn't exist, return false
+  return false;
 };
 
 console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCCED')) // true
