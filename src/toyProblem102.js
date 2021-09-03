8999700bdcb442a6da75811b4e6e057283f857c9
21grahams <graham.kirsh@gmail.com>
2021-09-03 09:12:04 -0600

    IOCE implemented

--- src/toyProblem102.js
+++ src/toyProblem102.js
@@ -4,6 +4,10 @@ Given an m x n grid of characters board and a string word, return true if word e
 
 The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once. */
 
+// input: an M x N grid of characters (in a matrix format) and a string of letters - word
+// output: a boolean - true if the word exists in the grid, false if it doesn't
+// constraints: m == board.length. n = board[i].length. 1 <= m, n <= 6. 1 <= word.length <= 15. board and word consists of only lowercase and uppercase English letters.
+// edge cases: if board is empty, return false
 const exist = (board, word) => {
   // do something
 };
