2327b8aa387f97fea068681b7949ba9e3db49ad0
21grahams <graham.kirsh@gmail.com>
2021-09-03 09:54:00 -0600

    initial pseudocode implemented

--- src/toyProblem102.js
+++ src/toyProblem102.js
@@ -9,7 +9,23 @@ The word can be constructed from letters of sequentially adjacent cells, where a
 // constraints: m == board.length. n = board[i].length. 1 <= m, n <= 6. 1 <= word.length <= 15. board and word consists of only lowercase and uppercase English letters.
 // edge cases: if board is empty, return false
 const exist = (board, word) => {
-  // do something
+  // create a rows, assign to length of matrix
+  // create a cols, assign to length of matrix at 0 index
+  // split the input word into a series of letters
+
+  // traverse board, loop over rows
+    // loop over cols
+      // loop over splitter
+        // while current letter in splitter exists on board
+          // increment splitter current element
+          // if next letter in splitter equals next vertical element in board (i 1 j 0)
+            // increment rows
+          // else if next letter in splitter equals next horizontal element in board (i 0 j 1)
+            // increment cols
+          // if we have reached end of splitter
+            // return true
+
+  // return false
 };
 
 console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCCED')) // true
