e42ad6f85798611ea9899debb805ed2b5dff304c
21grahams <graham.kirsh@gmail.com>
2021-09-05 12:50:43 -0600

    test suite implemented

--- src/toyProblem103.js
+++ src/toyProblem103.js
@@ -5,3 +5,15 @@ Given a string s, find the length of the longest substring without repeating cha
 const lengthOfLongestSubstring = s => {
   // do something
 };
+
+console.log(lengthOfLongestSubstring('abcabcbb')) // 3
+// Explanation: The answer is "abc", with the length of 3.
+
+console.log(lengthOfLongestSubstring('bbbbb')) // 1
+// Explanation: The answer is "b", with the length of 1.
+
+console.log(lengthOfLongestSubstring('pwwkew')) // 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+console.log(lengthOfLongestSubstring('')) // 0
