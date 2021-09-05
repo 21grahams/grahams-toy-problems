d048517fdb8804367882d32e108cad356695de60
21grahams <graham.kirsh@gmail.com>
2021-09-05 14:03:06 -0600

    updated pseudocode to work to implement using a new map

--- src/toyProblem103.js
+++ src/toyProblem103.js
@@ -7,29 +7,49 @@ Given a string s, find the length of the longest substring without repeating cha
 // constraints: 0 <= s.length <= 5 * 10^4. s consists of English letters, digits, symbols and spaces
 // edge cases: if the string is empty, return 0
 const lengthOfLongestSubstring = (s) => {
-  // account for edge cases
-  if (s.length === 0) return 0;
-  if (s.length === 1) return 1;
+  // // account for edge cases
+  // if (s.length === 0) return 0;
+  // if (s.length === 1) return 1;
 
-  // create a hash table object
-  // create a max length, assign to 0
-  // create a start, assign to 0
-  let hashMap = {}, max = 0, start = 0;
+  // // create a hash table object
+  // // create a max length, assign to 0
+  // // create a start, assign to 0
+  // let hashMap = {}, max = 0, start = 0;
 
-  // loop over s
-  for (let i = 0; i < s.length; i++) {
-    // if hash table object at current character is string is NOT undefined and hash table at current character is greater or equal to start
-    if (hashMap[s[i]] !== undefined && hashMap[s[i]] >= start) {
-      // assign start to be hash table at current character plus one
-      start = hashMap[s[i]] + 1;
-    }
-      // assign hash table at current character to be current index
-      hashMap[s[i]] = i;
-      // assign max to be Math.max of max and index minus start plus one
-      max = Math.max(max, (i - start + 1));
-  }
+  // // loop over s
+  // for (let i = 0; i < s.length; i++) {
+  //   // if hash table object at current character is string is NOT undefined and hash table at current character is greater or equal to start
+  //   if (hashMap[s[i]] !== undefined && hashMap[s[i]] >= start) {
+  //     // assign start to be hash table at current character plus one
+  //     start = hashMap[s[i]] + 1;
+  //   }
+  //     // assign hash table at current character to be current index
+  //     hashMap[s[i]] = i;
+  //     // assign max to be Math.max of max and index minus start plus one
+  //     max = Math.max(max, (i - start + 1));
+  // }
+  // // return max
+  // return max;
+
+  //===========================
+  // OPTION TWO USING A NEW MAP
+  //===========================
+
+  // create a letters var, assign to splitting s into array of letters
+  // create a max var, assign to 0
+  // create a result var, assign to new map
+  // create a start var, assign to 0
+
+  // loop over letters
+    // if result doesn't have curent letter
+      // set result with current letters
+    // otherwise
+      // assign index to result of getting current letter index
+      // clear result map
+    // if max is smaller than size of result
+      // assign max to size of result
+
   // return max
-  return max;
 };
 
 // Runtime: 132 ms, faster than 48.27% of JavaScript online submissions for Longest Substring Without Repeating Characters.
