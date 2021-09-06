3d8c02b57ef7762961e1bb0f6e3119127a5b0039
21grahams <graham.kirsh@gmail.com>
2021-09-06 10:22:00 -0600

    updated pseudocode v2

--- src/toyProblem104.js
+++ src/toyProblem104.js
@@ -9,16 +9,23 @@ Return the length of the longest substring containing the same letter you can ge
 // constraints: 1 <= s.length <= 10^5. s consists of only uppercase English letters. 0 <= k <= s.length
 // edge cases: none at this time, may add in something for either s or k being empty, but not right now
 const characterReplacement = (s, k) => {
-  // create a count var, assign to k
+  // create a left var, assign to 0
+  // create a right var, assign to 0
+  // create a maxCharCount var, assign to 0
+  // create a visited obj
 
-  // create a helperFunc called sort, takes in s
-    // assign len to length of s
-    // assign newArr to a new Array and pass in len
-    // loop over len
-      // assign newArr to equal the characterReplacement and pass in s and s[i]
-      // sort newArr
+  // while right is less than length of s
+    // create a char const, assign to s at right
+    // assign visited at char to equal result of visited at char either visited at char + 1 OR 1
 
-  // return newArr
+    // if visited at char is greater than maxCharCount, assign maxCharCount to visited at char
+
+    // if right minus left plus 1 minus maxCharCount is greater than k
+      // assign visited at s at left to decrement
+      // increment left
+    // increment right
+
+  // return right minus left
 };
 
 console.log(characterReplacement('ABAB', 2)) // 4
@@ -32,7 +39,10 @@ console.log(characterReplacement('AABABBA', 1)) // 4
 count = 0
 max = 0
 letters = ['A', 'B', 'A', 'B']
-result = {}
+result = {
+  A: 2,
+  B: 2
+}
 i =
 
 */
