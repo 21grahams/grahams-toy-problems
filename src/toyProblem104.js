b6623d54cd3ec5b82273a0baef7e1e434bd7896d
21grahams <graham.kirsh@gmail.com>
2021-09-06 09:23:23 -0600

    IOCE implemented

--- src/toyProblem104.js
+++ src/toyProblem104.js
@@ -4,6 +4,10 @@ You are given a string s and an integer k. You can choose any character of the s
 
 Return the length of the longest substring containing the same letter you can get after performing the above operations */
 
+// input: a string of characters (s) and an integer (k)
+// output: a single integer representing the longest possible substring of the input string that you can change only k amount of times
+// constraints: 1 <= s.length <= 10^5. s consists of only uppercase English letters. 0 <= k <= s.length
+// edge cases: none at this time, may add in something for either s or k being empty, but not right now
 const characterReplacement = (s, k) => {
   // do something
 };
