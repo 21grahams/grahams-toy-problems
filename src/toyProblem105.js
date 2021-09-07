c5fcb678739fafbf6c2ad4d683a35d2b944dfbe7
21grahams <graham.kirsh@gmail.com>
2021-09-07 09:23:45 -0600

    initial pseudocode implemented

--- src/toyProblem105.js
+++ src/toyProblem105.js
@@ -9,10 +9,23 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 // constraints: 1 <= strs.length <= 10^4. 0 <= strs[i].length <= 100. strs[i] consists of lowercase English letters.
 // edge cases: if the input is empty, return empty string. if the input only has one letter, return that letter
 const groupAnagrams = strs => {
-  // do something
+  // account for edge cases
+  // create a resultArr
+  // create a currentArr
+
+  // loop over strs array
+    // split into arrayOfLetters
+      // sort arrayOfLetters
+      // join arrayOfLetters back to words
+      // loop over each word starting at word 1
+        // if a word is the same as another
+          // add to currentArr
+          // spread into resultArr
+
+  // return resultArr
 };
 
-console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"])) // [["bat"],["nat","tan"],["ate","eat","tea"]]
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])) // [ ["bat"], ["nat", "tan"], ["ate", "eat", "tea"] ]
 
 console.log(groupAnagrams([''])) // [['']]
 
