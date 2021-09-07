5fac9cbe63c6b0632d423c96f12c265eda3da662
21grahams <graham.kirsh@gmail.com>
2021-09-07 09:16:19 -0600

    IOCE implemented

--- src/toyProblem105.js
+++ src/toyProblem105.js
@@ -4,6 +4,10 @@ Given an array of strings strs, group the anagrams together. You can return the 
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once */
 
+// input: an array of strings
+// output: the anagrams grouped together, inside a nested array
+// constraints: 1 <= strs.length <= 10^4. 0 <= strs[i].length <= 100. strs[i] consists of lowercase English letters.
+// edge cases: if the input is empty, return empty string. if the input only has one letter, return that letter
 const groupAnagrams = strs => {
   // do something
 };
