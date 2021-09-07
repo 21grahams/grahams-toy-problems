410b91a75e0e6b13176318fa1089e3a2b949d127
21grahams <graham.kirsh@gmail.com>
2021-09-07 09:49:37 -0600

    runtime 120ms, faster than 93% of online submissions

--- src/toyProblem105.js
+++ src/toyProblem105.js
@@ -30,6 +30,9 @@ const groupAnagrams = strs => {
   return Array.from(resultMap.values());
 };
 
+// Runtime: 120 ms, faster than 93.31% of JavaScript online submissions for Group Anagrams.
+// Memory Usage: 48.3 MB, less than 95.78% of JavaScript online submissions for Group Anagrams.
+
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])) // [ ["bat"], ["nat", "tan"], ["ate", "eat", "tea"] ]
 
 console.log(groupAnagrams([''])) // [['']]
