53f0b265f19175f928c74423345bb5a255923197
21grahams <graham.kirsh@gmail.com>
2021-09-08 13:16:28 -0600

    test suite implemented

--- src/toyProblem106.js
+++ src/toyProblem106.js
@@ -5,3 +5,11 @@ Given a string s, return the longest palindromic substring in s */
 const longestPalindrome = s => {
   // do something
 };
+
+console.log(longestPalindrome('babad')) // 'bab' ('aba' is also a valid answer)
+
+console.log(longestPalindrome('cbbd')) // 'bb'
+
+console.log(longestPalindrome('a')) // 'a'
+
+console.log(longestPalindrome('ac')) // 'a'
