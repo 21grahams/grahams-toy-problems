c934ffae5b4341e4c271fc8fef1b89f7e77603cc
21grahams <graham.kirsh@gmail.com>
2021-09-09 15:28:48 -0600

    IOCE implemented

--- src/toyProblem106.js
+++ src/toyProblem106.js
@@ -2,6 +2,10 @@
 
 Given a string s, return the longest palindromic substring in s */
 
+// input: a string, labeled as s
+// output: a string, which represents the longest substring palindrome in s
+// constraints: 1 <= s.length <= 1000. s consist of only digits and English letters.
+// edge cases: if string is empty, return empty null. If string contains just one letter, return that letter
 const longestPalindrome = s => {
   // do something
 };
