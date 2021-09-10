f6cea49718789fff555532b5821bbc74ff62176d
21grahams <graham.kirsh@gmail.com>
2021-09-10 12:17:17 -0600

    initial pseudocode implemented

--- src/toyProblem106.js
+++ src/toyProblem106.js
@@ -7,7 +7,16 @@ Given a string s, return the longest palindromic substring in s */
 // constraints: 1 <= s.length <= 1000. s consist of only digits and English letters.
 // edge cases: if string is empty, return empty null. If string contains just one letter, return that letter
 const longestPalindrome = s => {
-  // do something
+  // account for edge case
+
+  // split s into an array of letters
+  // create a result, assign as a string
+
+  // create a helperFunc using recursion
+    // base case - if letter length is 1, increment result and letter
+
+    // recursive case - if letters reversed joined doesn't equal arrayOfLetters
+      // call recursive function with slice
 };
 
 console.log(longestPalindrome('babad')) // 'bab' ('aba' is also a valid answer)
