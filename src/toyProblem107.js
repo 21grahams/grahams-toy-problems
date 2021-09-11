d19e72c729a3e9b154be5a7dfd328e1c3c6c1afc
21grahams <graham.kirsh@gmail.com>
2021-09-11 10:17:28 -0600

    test suite implemented

--- src/toyProblem107.js
+++ src/toyProblem107.js
@@ -9,3 +9,9 @@ A substring is a contiguous sequence of characters within the string */
 const countSubstrings = s => {
   // do something
 };
+
+console.log(countSubstrings('abc')) // 3
+// Explanation: Three palindromic strings: "a", "b", "c"
+
+console.log(countSubstrings('aaa')) // 6
+// Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa"
