479dc0aae578a10f566b39b93f472d4fd58c8153
21grahams <graham.kirsh@gmail.com>
2021-09-13 08:52:23 -0600

    test suite implemented

--- src/toyProblem108.js
+++ src/toyProblem108.js
@@ -5,3 +5,9 @@ Given the root of a binary tree, invert the tree, and return its root */
 const invertTree = root => {
   // do something
 };
+
+console.log(invertTree([4, 2, 7, 1, 3, 6, 9])) // [4, 7, 2, 9, 6, 3, 1]
+
+console.log(invertTree([2, 1, 3])) // [2, 3, 1]
+
+console.log(invertTree([])) // []
