9f01c4e218abfd37512e721da74e6fcf436ec822
21grahams <graham.kirsh@gmail.com>
2021-09-13 09:10:36 -0600

    initial pseudocode implemented

--- src/toyProblem108.js
+++ src/toyProblem108.js
@@ -6,12 +6,63 @@ Given the root of a binary tree, invert the tree, and return its root */
 // output: the same binary tree, inverted, starting with the root
 // constraints: The number of nodes in the tree is in the range [0, 100]. -100 <= Node.val <= 100
 // edge cases: if the root is empty, return an empty tree (empty array)
-const invertTree = root => {
-  // do something
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  };
 };
 
-console.log(invertTree([4, 2, 7, 1, 3, 6, 9])) // [4, 7, 2, 9, 6, 3, 1]
+const invertTree = root => {
+  // account for edge case
 
-console.log(invertTree([2, 1, 3])) // [2, 3, 1]
+  // create a node var, assign to root
+  // create a queue var, assign to [root]
+  // while node exists
+    // create a cur variable, assign to length of queue
+    // loop over cur
+      // shift out first node in queue
+      // if node at left exists
+        // push to queue.right
+      // if node at right exists
+        // push to queue.left
 
+  // return queue
+};
+
+// test one
+let nodeA = new TreeNode(4);
+let nodeB = new TreeNode(2);
+let nodeC = new TreeNode(7);
+let nodeD = new TreeNode(1);
+let nodeE = new TreeNode(3);
+let nodeF = new TreeNode(6);
+let nodeG = new TreeNode(9);
+
+nodeA.left = nodeB;
+nodeA.right = nodeC;
+nodeB.left = nodeD;
+nodeB.right = nodeE;
+nodeC.left = nodeF;
+nodeC.right = nodeG;
+
+console.log(invertTree(nodeA)) // [4, 7, 2, 9, 6, 3, 1]
+
+// test two
+let nodeX = new TreeNode(2);
+let nodeY = new TreeNode(1);
+let nodeZ = new TreeNode(3);
+
+nodeX.left = nodeY;
+nodeX.right = nodeZ;
+nodeY.left = null;
+nodeY.right = null;
+nodeZ.left = null;
+nodeZ.right = null;
+
+console.log(invertTree(nodeX)) // [2, 3, 1]
+
+// test three
 console.log(invertTree([])) // []
