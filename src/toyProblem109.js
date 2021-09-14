552d013caec2afce21f9d96a078e8313f72ee7f5
21grahams <graham.kirsh@gmail.com>
2021-09-14 10:14:23 -0500

    problem init - Character Numeral Swap

---
+++ src/toyProblem109.js
@@ -0,0 +1,7 @@
+/* Character Numeral Swap
+
+Given an input string of different characters, return the string with a swap of vowels to their numeric counterparts */
+
+const charSwap = s => {
+  // do something
+};
