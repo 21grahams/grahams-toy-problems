e1036894d5153c056893bd0c664693ae1be9d90f
21grahams <graham.kirsh@gmail.com>
2021-09-14 10:17:21 -0500

    test suite implemented

--- src/toyProblem109.js
+++ src/toyProblem109.js
@@ -5,3 +5,11 @@ Given an input string of different characters, return the string with a swap of 
 const charSwap = s => {
   // do something
 };
+
+console.log(charSwap('Avatar')) // '1v1t1r'
+
+console.log(charSwap('Hack Reactor')) // 'H1ck R21ct4r'
+
+console.log(charSwap('Galvanize')) // 'G1lv1n3z2'
+
+console.log(charSwap('')) // ''
