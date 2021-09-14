91f5fa3fdfa88813ceb25830132531ee60c2a46d
21grahams <graham.kirsh@gmail.com>
2021-09-14 10:19:22 -0500

    IOCE implemented

--- src/toyProblem109.js
+++ src/toyProblem109.js
@@ -2,6 +2,10 @@
 
 Given an input string of different characters, return the string with a swap of vowels to their numeric counterparts */
 
+// input: a string of english letter characters
+// output: the same string, replacing all vowels with their numeric equivalents
+// constraints: none at this time
+// edge cases: if string is empty, return null
 const charSwap = s => {
   // do something
 };
@@ -12,4 +16,4 @@ console.log(charSwap('Hack Reactor')) // 'H1ck R21ct4r'
 
 console.log(charSwap('Galvanize')) // 'G1lv1n3z2'
 
-console.log(charSwap('')) // ''
+console.log(charSwap('')) // null
