3b4356a70146cd118250750f13228a94b554ad85
21grahams <graham.kirsh@gmail.com>
2021-09-14 10:39:28 -0500

    optimized solution pseudocode v1 implemented

--- src/toyProblem109.js
+++ src/toyProblem109.js
@@ -9,34 +9,49 @@ Given an input string of different characters, return the string with a swap of 
 const charSwap = s => {
   // naive solution
 
-  // account for egde case
-  // create a resultStr
-  if (s.length === 0) return null;
-  let resultStr = '';
+  // // account for egde case
+  // // create a resultStr
+  // if (s.length === 0) return null;
+  // let resultStr = '';
 
-  // loop over string
-  for (let i = 0; i < s.length; i++) {
-    // if current element of string equals 'A' or 'a'
-    // increment 1 to resultStr
-    if (s[i] === 'A' || s[i] === 'a') resultStr += '1';
-    // else if current element of string equals 'E' or 'e'
-    // increment 2 to resultStr
-    else if (s[i] === 'E' || s[i] === 'e') resultStr += '2';
-    // else if current element of string equals 'I' or 'i'
-    // increment 3 to resultStr
-    else if (s[i] === 'I' || s[i] === 'i') resultStr += '3';
-    // else if current element of string equals 'O' or 'o'
-    // increment 4 to resultStr
-    else if (s[i] === 'O' || s[i] === 'o') resultStr += '4';
-    // else if current element of string equals 'U' or 'u'
-    // increment 5 to resultStr
-    else if (s[i] === 'U' || s[i] === 'u') resultStr += '5';
-    // else
-    // add current element to resultStr
-    else resultStr += s[i];
-  }
+  // // loop over string
+  // for (let i = 0; i < s.length; i++) {
+  //   // if current element of string equals 'A' or 'a'
+  //   // increment 1 to resultStr
+  //   if (s[i] === 'A' || s[i] === 'a') resultStr += '1';
+  //   // else if current element of string equals 'E' or 'e'
+  //   // increment 2 to resultStr
+  //   else if (s[i] === 'E' || s[i] === 'e') resultStr += '2';
+  //   // else if current element of string equals 'I' or 'i'
+  //   // increment 3 to resultStr
+  //   else if (s[i] === 'I' || s[i] === 'i') resultStr += '3';
+  //   // else if current element of string equals 'O' or 'o'
+  //   // increment 4 to resultStr
+  //   else if (s[i] === 'O' || s[i] === 'o') resultStr += '4';
+  //   // else if current element of string equals 'U' or 'u'
+  //   // increment 5 to resultStr
+  //   else if (s[i] === 'U' || s[i] === 'u') resultStr += '5';
+  //   // else
+  //   // add current element to resultStr
+  //   else resultStr += s[i];
+  // }
+  // // return resultStr
+  // return resultStr;
+
+  // optimized solution
+
+  // account for edge case
+  // create a resultStr
+  // create a new Map
+  // lower case all letters
+
+  // set a => 1, e => 2, i => 3, o => 4 u => 5 to the map
+  // loop over s
+    // if current element of s of new map has key
+      // add resultStr with it's value
+
+  // uppercase the first letter of each word
   // return resultStr
-  return resultStr;
 };
 
 console.log(charSwap('Avatar')) // '1v1t1r'
