3ec45e62e7d639166d099c079f5611f2b4991cc8
21grahams <graham.kirsh@gmail.com>
2021-09-16 09:26:11 -0600

    solution implemented and passing all leetcode tests

--- src/toyProblem110.js
+++ src/toyProblem110.js
@@ -5,28 +5,46 @@ Given the root of a binary tree, return the level order traversal of its nodes' 
 // input: the root of a binary tree
 // output: the level order traversal of the binary tree input (BFS)
 // constraints: The number of nodes in the tree is in the range [0, 2000]. -1000 <= Node.val <= 1000
-// edge cases: if there's no root, return an empty array
+// edge cases: if there's no root, return null
 class TreeNode {
   constructor(val) {
     this.val = val;
     this.left = null;
     this.right = null;
-  };
-};
+  }
+}
 
-const levelOrder = root => {
+const levelOrder = (root) => {
   // account for edge case
+  if (!root) return [];
 
   // create a queue, assign to [root]
-  // create a node, assign to root
-  // while root exists (queue.length)
-    // assign node to a removal (shift) of queue
-    // create a cur var, assign length of queue
-    // loop over cur
-      // if there's a left value, push it to queue
-      // if there's a right value, push it to queue
+  // create a result, assign to []
+  let queue = [root],
+      result = [];
 
-  // return root
+  // while root exists (queue.length)
+  while (queue.length) {
+    // create length var, assign to length of queue
+    // create node var, assign to empty array
+    let length = queue.length;
+    let node = [];
+    // loop over length
+    for (let i = 0; i < length; i++) {
+      // create cur, assign to queue shift
+      let cur = queue.shift();
+      // push cur value onto node array
+      node.push(cur.val);
+      // if there's a left value, push it to queue
+      if (cur.left) queue.push(cur.left);
+      // if there's a right value, push it to queue
+      if (cur.right) queue.push(cur.right);
+    }
+    // push all nodes onto result array
+    result.push(node);
+  }
+  // return result
+  return result;
 };
 
 // test one
@@ -47,7 +65,7 @@ nodeD.right = null;
 nodeE.left = null;
 nodeE.right = null;
 
-console.log(levelOrder(nodeA)) // [[3], [9, 20], [15, 7]]
+console.log(levelOrder(nodeA)); // [[3], [9, 20], [15, 7]]
 
 // test two
 let nodeG = new TreeNode(1);
@@ -55,7 +73,7 @@ let nodeG = new TreeNode(1);
 nodeG.left = null;
 nodeG.right = null;
 
-console.log(levelOrder(nodeG)) // [[1]]
+console.log(levelOrder(nodeG)); // [[1]]
 
 // test three
-console.log(levelOrder()) // []
+console.log(levelOrder()); // []
