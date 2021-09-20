0f06eeceff2b26028f40c258afc9893ad5e34a2c
21grahams <graham.kirsh@gmail.com>
2021-09-20 09:31:36 -0600

    solution implemented and passing all leetcode tests

--- src/toyProblem112.js
+++ src/toyProblem112.js
@@ -16,39 +16,33 @@ class TreeNode {
 
 const buildTree = (preorder, inorder) => {
   // create a new map
+  let map = new Map();
   // loop over inorder with a forEach, takes in element and index
+  inorder.forEach((element, index) => {
     // set element and index to map
-
+    map.set(element, index);
+  })
   // create a helper function, takes in start and end
+  function helper(start, end) {
     // if start is greater than end, return null
+    if (start > end) return null;
     // create a curr var, assign to preorder.shift()
+    let curr = preorder.shift();
     // create a node var, assign to new TreeNode(curr)
+    let node = new TreeNode(curr);
     // assign left node to helper function, takes in start, map.get on curr - 1
-    // assign left node to helper function, takes in map.get on curr + 1 and end
+    node.left = helper(start, map.get(curr) - 1);
+    // assign right node to helper function, takes in map.get on curr + 1 and end
+    node.right = helper(map.get(curr) + 1, end);
     // return node
-
+    return node;
+  }
   // return helper, takes in 0 and inorder.length - 1
+  return helper(0, inorder.length - 1)
 };
 
 // test one
-let nodeA = new TreeNode(3);
-let nodeB = new TreeNode(9);
-let nodeC = new TreeNode(20);
-let nodeD = new TreeNode(15);
-let nodeE = new TreeNode(7);
-
-nodeA.left = nodeB;
-nodeA.right = nodeC;
-nodeB.left = null;
-nodeB.right = null;
-nodeC.left = nodeD;
-nodeC.right = nodeE;
-nodeD.left = null;
-nodeD.right = null;
-nodeE.left = null;
-nodeE.right = null;
-
-console.log(buildTree(nodeA, nodeA)) // [3, 9, 20, null, null, 15, 7]
+console.log(buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7])) // [3, 9, 20, null, null, 15, 7]
 
 // test two
 console.log(buildTree([-1], [-1])) // [-1]
