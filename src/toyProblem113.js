96997f0d853d5125040ed4d6626c7caf3d26d277
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-10-20 22:22:35 -0600

    test suite implemented

--- src/toyProblem113.js
+++ src/toyProblem113.js
@@ -6,6 +6,16 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order */
 
-const twoSum = () => {
+const twoSum = (nums, target) => {
     // do something
-}
+};
+
+
+console.log(twoSum([2, 7, 11, 15], 9)) // [0, 1]
+// explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+console.log(twoSum([3, 2, 4], 5)) // [1, 2]
+
+console.log(twoSum([3, 3], 6)) // [0, 1]
+Input: nums = [3,3], target = 6
+Output: [0,1]
