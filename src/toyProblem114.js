8d26e4a4f0aa4dedf258e9c5404024f53a3ae00a
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-10-24 22:49:26 -0600

    IOCE implemented

--- src/toyProblem114.js
+++ src/toyProblem114.js
@@ -1,6 +1,8 @@
 /* leetcode - Minimum Number of Moves to Seat Everyone
 
-There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, 
+
+where students[j] is the position of the jth student.
 
 You may perform the following move any number of times:
 
@@ -9,6 +11,10 @@ Return the minimum number of moves required to move each student to a seat such 
 
 Note that there may be multiple seats or students in the same position at the beginning */
 
+// input: n number of seats and x number of students represented as separate arrays
+// output: the minimum number of moves required to move each student to a seat such that no two students are in the same seat
+// constaints: n == seats.length == students.length. 1 <= n <= 100. 1 <= seats[i], students[j] <= 100
+// edge cases: none at this time
 const minMovesToSeat = (seats, students) => {
   // do something  
 };
