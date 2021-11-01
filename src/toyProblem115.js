ca6fffb2a37c8e874fc1afa85b05b9c7f1cbf8ac
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-10-31 23:09:05 -0600

    test suite implemented

--- src/toyProblem115.js
+++ src/toyProblem115.js
@@ -9,3 +9,8 @@ You may assume that each input would have exactly one solution */
 const threeSumClosest = (nums, target) => {
   // do something  
 };
+
+console.log(threeSumClosest([-1, 2, 1, -4], 1)) // 2
+// The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+console.log(threeSumClosest([0, 0, 0], 1)) // 0
