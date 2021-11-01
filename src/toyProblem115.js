9b80314be191cf9dccbf2233948b325c3a4a2cc0
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-10-31 23:10:56 -0600

    IOCE implemented

--- src/toyProblem115.js
+++ src/toyProblem115.js
@@ -6,6 +6,10 @@ Return the sum of the three integers.
 
 You may assume that each input would have exactly one solution */
 
+// input: an array of integers, positive or negative and an integer that's a target number
+// output: the sum of the three integers that sum closest to the target
+// constaints: 3 <= nums.length <= 1000. -1000 <= nums[i] <= 1000. -104 <= target <= 104
+// edge cases: none at this time
 const threeSumClosest = (nums, target) => {
   // do something  
 };
