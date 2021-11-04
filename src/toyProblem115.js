64e6ca19d600ab3f1dc40c57e8aee2f43e09a769
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-11-04 04:29:10 -0600

    updated pseudocode implemented

--- src/toyProblem115.js
+++ src/toyProblem115.js
@@ -11,32 +11,17 @@ You may assume that each input would have exactly one solution */
 // constaints: 3 <= nums.length <= 1000. -1000 <= nums[i] <= 1000. -104 <= target <= 104
 // edge cases: none at this time
 const threeSumClosest = (nums, target) => {
-  
-  /* we have to establish several things: 
-    1. The closest number to the target given a combination of every three sets of integers
-    2. A way to figure out what number is closest to target. Can we consider a range of numbers that the closest would fall within? Ex:
-      exact = target
-      withinOneUp = target + 1
-      withinOneDown = target - 1
-      withinTwoUp = target + 2
-      withinTwoDown = target - 2
-      etc
-  */
 
-  // create several variables ranging from 3 under target to 3 above target
+  // create closestToTarget variable
   // create sum var, assign to 0
+  let closestToTarget = 0, sum = 0;
 
-  // loop over input array
-    // loop a second time with first array index plus one
-      // if sum of first three ints equals withinOneUp
-        // add to sum
-      // else if sum of first three ints equals withinOneDown
-        // add to sum
-      // else if sum of first three ints equals withinTwoUp
-        // add to sum
-      // else if sum of first three ints equals withinTwoDown
-        // add to sum
-
+  // sort input array then loop
+    // collect sum of first three values
+      // store in closestToTarget variable
+        // repeat process for all combinations of 3 ints
+        // if any three ints are closer to target
+          // add those 3 to sum
   
   // return sum
 };
