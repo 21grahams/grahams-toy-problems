cf3bf1fc51fb1f3a4882cb1c2683e311c26baee7
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-11-14 22:31:21 -0700

    test suite implemented

--- src/toyProblem116.js
+++ src/toyProblem116.js
@@ -7,3 +7,13 @@ An integer is a palindrome when it reads the same backward as forward. For examp
 const isPalindrome = x =>{
   // do something  
 };
+
+console.log(isPalindrome(121)) // true
+
+console.log(isPalindrome(-121)) // false
+// explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+console.log(isPalindrome(10)) // false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+console.log(isPalindrome(-101)) // false
