2beadd2c3d77ccf17432743cda628c892ed4785a
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-11-14 22:32:39 -0700

    IOCE implemented

--- src/toyProblem116.js
+++ src/toyProblem116.js
@@ -4,6 +4,10 @@ Given an integer x, return true if x is palindrome integer.
 
 An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not. */
 
+// input: an interger (x)
+// output: a boolean, true if integer is a palindrome, false if not
+// constraints: -231 <= x <= 231 - 1
+// edge cases: if integer is single digit, return true
 const isPalindrome = x =>{
   // do something  
 };
