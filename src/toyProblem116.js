d44fe8143b33e6d34c21d9b722163356ddc54b46
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-11-14 22:29:16 -0700

    leetcode init - Palindrome Number

---
+++ src/toyProblem116.js
@@ -0,0 +1,9 @@
+/* leetcode - Palindrome Number
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not. */
+
+const isPalindrome = x =>{
+  // do something  
+};
