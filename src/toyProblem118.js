37a7a0e8ba5c062d62f2f30c9c61e12ccf97ec2a
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-11-25 14:12:46 -0600

    test suite implemented

--- src/toyProblem118.js
+++ src/toyProblem118.js
@@ -7,3 +7,15 @@ An integer n is a power of two, if there exists an integer x such that n == 2x *
 const isPowerOfTwo = n => {
   // do something  
 };
+
+console.log(isPowerOfTwo(1)) // true
+// Explanation: 20 = 1
+
+console.log(isPowerOfTwo(16)) // true
+// Explanation: 24 = 16
+
+console.log(isPowerOfTwo(3)) // false
+
+console.log(isPowerOfTwo(4)) // true
+
+console.log(isPowerOfTwo(5)) // false
