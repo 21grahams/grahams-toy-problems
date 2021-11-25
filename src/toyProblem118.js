33926046b9c4619d42998d15bff5c21b23267b11
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-11-25 14:21:04 -0600

    IOCE implemented IOCE implemented IOCE implemented IOCE implemented IOCE implemented IOCE implemented IOCE implemented IOCE implemented IOCE implemented

--- src/toyProblem118.js
+++ src/toyProblem118.js
@@ -4,15 +4,19 @@ Given an integer n, return true if it is a power of two. Otherwise, return false
 
 An integer n is a power of two, if there exists an integer x such that n == 2x */
 
+// input: an integer (n)
+// output: a boolean, true if n is a power of two, false if not
+// constraints: -231 <= n <= 231 - 1
+// edge cases: none at this time
 const isPowerOfTwo = n => {
   // do something  
 };
 
 console.log(isPowerOfTwo(1)) // true
-// Explanation: 20 = 1
+// Explanation: 2^0 = 1
 
 console.log(isPowerOfTwo(16)) // true
-// Explanation: 24 = 16
+// Explanation: 2^4 = 16
 
 console.log(isPowerOfTwo(3)) // false
 
