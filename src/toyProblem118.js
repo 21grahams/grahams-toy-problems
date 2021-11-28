b1f5aec45ca6eb81e3a9ae84c097d9e4a98f7e77
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-11-28 11:11:17 -0700

    solution implemented and passing all leetcode tests

--- src/toyProblem118.js
+++ src/toyProblem118.js
@@ -8,28 +8,34 @@ An integer n is a power of two, if there exists an integer x such that n == 2x *
 // output: a boolean, true if n is a power of two, false if not
 // constraints: -231 <= n <= 231 - 1
 // edge cases: if n is 1, return true
-const isPowerOfTwo = n => {
+const isPowerOfTwo = (n) => {
   // account for edge case
-
+  if (n <= 0) return false;
   // set a var for num, assign to n
-  // while num is less than or equal to 1
+  let num = n;
+  // while num is greater than or equal to 1
+  while (num >= 1) {
     // if num is equal to 1
-      // return true
+    // return true
+    if (num === 1) return true;
     // if num modulo 2 is not 0
-      // return false
-      // assign num to Math.floor of num / 2
+    // return false
+    if (num % 2 !== 0) return false;
+    // assign num to Math.floor of num / 2
+    num = Math.floor(num / 2);
+  }
 };
 
-console.log(isPowerOfTwo(1)) // true
+console.log(isPowerOfTwo(1)); // true
 // Explanation: 2^0 = 1
 
-console.log(isPowerOfTwo(16)) // true
+console.log(isPowerOfTwo(16)); // true
 // Explanation: 2^4 = 16
 
-console.log(isPowerOfTwo(3)) // false
+console.log(isPowerOfTwo(3)); // false
 
-console.log(isPowerOfTwo(4)) // true
+console.log(isPowerOfTwo(4)); // true
 
-console.log(isPowerOfTwo(5)) // false
+console.log(isPowerOfTwo(5)); // false
 
-console.log(isPowerOfTwo(6)) // false
+console.log(isPowerOfTwo(6)); // false
