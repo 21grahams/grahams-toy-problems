0710f9a031bdaeceae059c9da4f0c54246d8163d
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-12-11 14:25:48 -0700

    toy problem init - fashion catalog

---
+++ src/toyProblem121.js
@@ -0,0 +1,33 @@
+/* You have a fashion catalog, an inventory of items from various high-fashion designers. Each designer has a lineup of shoes. Each shoe has a name and a price.
+
+It looks like this:
+
+var currentInventory = [
+  {
+    name: 'Brunello Cucinelli',
+    shoes: [
+      {name: 'tasselled black low-top lace-up', price: 1000},
+      {name: 'tasselled green low-top lace-up', price: 1100},
+      {name: 'plain beige suede moccasin', price: 950},
+      {name: 'plain olive suede moccasin', price: 1050}
+    ]
+  },
+  {
+    name: 'Gucci',
+    shoes: [
+      {name: 'red leather laced sneakers', price: 800},
+      {name: 'black leather laced sneakers', price: 900}
+    ]
+  }
+];
+
+Write a function that will take in this currentInventory array as its argument. Your function should access all the shoes across each designer and return them out in a flat list: {designer name} - {shoe name} - {price}{designer name} - {shoe name} - {price}
+
+//...console output:
+Brunello Cucinelli, tasselled black low-top lace-up, 1000
+Brunello Cucinelli, tasselled green low-top lace-up, 1100
+// and so on...*/
+
+const renderInventory = currentInventory => {
+  // do something
+}
