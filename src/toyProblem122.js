8b6236d7ad39fa4067dbd682ee458342491efd85
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-12-12 12:19:35 -0700

    test suite implemented

--- src/toyProblem122.js
+++ src/toyProblem122.js
@@ -37,8 +37,41 @@ var expected = {
       'averagePrice': 850
     }
   ]
-}*/
+} */
 
 const calculateAveragePricePerDesigner = inventory => {
   // do something
 }
+
+var currentInventory = [
+  {
+    name: 'Brunello Cucinelli',
+    shoes: [
+      {name: 'tasselled black low-top lace-up', price: 1000},
+      {name: 'tasselled green low-top lace-up', price: 1100},
+      {name: 'plain beige suede moccasin', price: 950},
+      {name: 'plain olive suede moccasin', price: 1050}
+    ]
+  },
+  {
+    name: 'Gucci',
+    shoes: [
+      {name: 'red leather laced sneakers', price: 800},
+      {name: 'black leather laced sneakers', price: 900}
+    ]
+  }
+]
+
+console.log(calculateAveragePricePerDesigner(inventory)) // should output:
+/*  {
+  'designers': [
+    {
+      'name': 'Brunello Cucinelli',
+      'averagePrice': 1025
+    },
+    {
+      'name': 'Gucci',
+      'averagePrice': 850
+    }
+  ]
+} */
