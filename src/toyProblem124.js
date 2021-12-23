fc6f835b3239e56ec92787f834b3887d06b9d360
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-12-23 10:33:23 -0700

    solution implemented and passing all tests

--- src/toyProblem124.js
+++ src/toyProblem124.js
@@ -65,43 +65,74 @@ var expectedResult = [
 ]; */
 
 // input: a nested array
-// output: an array containing all the laced shoes that contain "lace" in them, 
+// output: an array containing all the laced shoes that contain "lace" in them,
 //   and indicate which word contains "lace"
 // constraints: none at this time
 // edge cases: none at this time
-const fashionInventory = renderInventory => {
+const fashionInventory = (renderInventory) => {
   // create a resultArr
+  let resultArr = [];
   // create nameWords object, containing array
+  let resultObj = { nameWords: [] };
   // create targetWordIndex string
+  let targetWordIndex = "";
   // loop over renderInventory array
+  for (let i = 0; i < renderInventory.length; i++) {
     // create variable for shoes
+    let shoes = renderInventory[i].shoes;
     // loop over shoes array
+    for (let j = 0; j < shoes.length; j++) {
       // create variable for designerDescription
-      // add designerDescription to nameWords array
-      // find index of the word lace and add to targetWordIndex
-  // return resultArr
-}
+      let designerDescription = shoes[j].name;
+      let designerDescriptionArr = designerDescription.split(" ");
+      // add designerDescription to nameWords array if it contains lace
+      let laceObj = {};
+      if (containsLace(shoes[j])) {
+        laceObj.nameWords = designerDescriptionArr;
+        laceObj.targetWordIndex = indexOfLace(designerDescriptionArr);
+        resultArr.push(laceObj);
+      }
+    }
+  }
+  return resultArr;
+};
+
+const containsLace = (lace) => {
+  if (lace.name.indexOf("lace") !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const indexOfLace = (idx) => {
+  for (let i = 0; i < idx.length; i++) {
+    if (idx[i].indexOf("lace") !== -1) {
+      return i;
+    }
+  }
+};
 
 var currentInventory = [
   {
-    name: 'Brunello Cucinelli',
+    name: "Brunello Cucinelli",
     shoes: [
-      {name: 'tasselled black low-top lace-up', price: 1000},
-      {name: 'tasselled green low-top lace-up', price: 1100},
-      {name: 'plain beige suede moccasin', price: 950},
-      {name: 'plain olive suede moccasin', price: 1050}
-    ]
+      { name: "tasselled black low-top lace-up", price: 1000 },
+      { name: "tasselled green low-top lace-up", price: 1100 },
+      { name: "plain beige suede moccasin", price: 950 },
+      { name: "plain olive suede moccasin", price: 1050 },
+    ],
   },
   {
-    name: 'Gucci',
+    name: "Gucci",
     shoes: [
-      {name: 'red leather laced sneakers', price: 800},
-      {name: 'black leather laced sneakers', price: 900}
-    ]
-  }
-]
+      { name: "red leather laced sneakers", price: 800 },
+      { name: "black leather laced sneakers", price: 900 },
+    ],
+  },
+];
 
-console.log(fashionInventory(currentInventory))
+console.log(fashionInventory(currentInventory));
 /*
 var expectedResult = [
   {
