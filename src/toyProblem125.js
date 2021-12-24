c7fe984383d58a25757cac4edd5c947f9f98c151
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-12-24 10:17:56 -0700

    toyProblem init - decorateClassListWithAges

---
+++ src/toyProblem125.js
@@ -0,0 +1,10 @@
+/* Write a function called decorateClassListWithAges. Convert from an array, in this case a class list of names, to an array of object literals decorated with a name and an age for each student. Your decorateClassListWithAges function should use the provided getRandomIntInclusive as part of its solution. 
+(The ages should be randomly generated for each student, either age 10 or age 11)
+
+var classList = ["Joe", "Jack", "John", "Fred", "Frank", "Barry", "Larry", "Mary",
+"Harry", "Farrell", "Susan", "Monica", "Keira", "Caroline", "Harriet", "Erica",
+"Luann", "Cheryl", "Beth", "Rupa", "Linda", "Allison", "Nancy", "Dora"] */
+
+const decorateClassListWithAges = () => {
+  // do something
+}
