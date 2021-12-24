8aa75cdba2317981b5dcfa32ff570e30a1496dbb
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-12-24 11:27:07 -0700

    test suite implemented

--- src/toyProblem125.js
+++ src/toyProblem125.js
@@ -5,6 +5,21 @@ var classList = ["Joe", "Jack", "John", "Fred", "Frank", "Barry", "Larry", "Mary
 "Harry", "Farrell", "Susan", "Monica", "Keira", "Caroline", "Harriet", "Erica",
 "Luann", "Cheryl", "Beth", "Rupa", "Linda", "Allison", "Nancy", "Dora"] */
 
-const decorateClassListWithAges = () => {
+const decorateClassListWithAges = students => {
   // do something
 }
+
+var classList = ["Joe", "Jack", "John", "Fred", "Frank", "Barry", "Larry", "Mary",
+"Harry", "Farrell", "Susan", "Monica", "Keira", "Caroline", "Harriet", "Erica",
+"Luann", "Cheryl", "Beth", "Rupa", "Linda", "Allison", "Nancy", "Dora"]
+
+console.log(decorateClassListWithAges(classList)) /*
+[{"name":"Joe","age":11},{"name":"Jack","age":10},
+{"name":"John","age":11},{"name":"Fred","age":11},{"name":"Frank","age":11},
+{"name":"Barry","age":11},{"name":"Larry","age":11},{"name":"Mary","age":11},
+{"name":"Harry","age":11},{"name":"Farrell","age":10},{"name":"Susan","age":10},
+{"name":"Monica","age":11},{"name":"Keira","age":10},{"name":"Caroline","age":10},
+{"name":"Harriet","age":11},{"name":"Erica","age":11},{"name":"Luann","age":10},
+{"name":"Cheryl","age":11},{"name":"Beth","age":10},{"name":"Rupa","age":11},
+{"name":"Linda","age":10},{"name":"Allison","age":10},{"name":"Nancy","age":10},
+{"name":"Dora","age":10}] */
