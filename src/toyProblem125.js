88eb8b2da29331d7636bc9553ddc2086b3543a7e
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-12-26 22:40:24 -0700

    IOCE implemented

--- src/toyProblem125.js
+++ src/toyProblem125.js
@@ -1,10 +1,16 @@
-/* Write a function called decorateClassListWithAges. Convert from an array, in this case a class list of names, to an array of object literals decorated with a name and an age for each student. Your decorateClassListWithAges function should use the provided getRandomIntInclusive as part of its solution. 
+/* Write a function called decorateClassListWithAges. Convert from an array, in this case a class list of names, 
+to an array of object literals decorated with a name and an age for each student. Your decorateClassListWithAges 
+function should use the provided getRandomIntInclusive as part of its solution. 
 (The ages should be randomly generated for each student, either age 10 or age 11)
 
 var classList = ["Joe", "Jack", "John", "Fred", "Frank", "Barry", "Larry", "Mary",
 "Harry", "Farrell", "Susan", "Monica", "Keira", "Caroline", "Harriet", "Erica",
 "Luann", "Cheryl", "Beth", "Rupa", "Linda", "Allison", "Nancy", "Dora"] */
 
+// input: an array of strings
+// output: an object literal of each string and an age associated with that name (string)
+// constraints: (The ages should be randomly generated for each student, either age 10 or age 11)
+// edge cases: none at this time
 const decorateClassListWithAges = students => {
   // do something
 }
