a9e0c5c29116ab4c88a219a5304c033c2941a2c2
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-01 15:27:52 -0700

    test suite implemented

--- src/toyProblem126.js
+++ src/toyProblem126.js
@@ -5,3 +5,6 @@ Given the head of a singly linked list, return true if it is a palindrome */
 const isPalindrome = head => {
   // do something  
 };
+
+console.log(isPalindrome([1, 2, 2, 1])) // true
+console.log(isPalindrome([1, 2])) // false
