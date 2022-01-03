43d67e008cae41ed803b34e19fc750ee04bb2e48
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-02 22:11:36 -0700

    IOCE implemented

--- src/toyProblem126.js
+++ src/toyProblem126.js
@@ -2,6 +2,10 @@
 
 Given the head of a singly linked list, return true if it is a palindrome */
 
+// input: the head of a singly linked list
+// output: a boolean, true if the singly linked list is a palindrome, false if not
+// constraints: The number of nodes in the list is in the range [1, 105]. 0 <= Node.val <= 9
+// edge cases: if head has one number only, return true. If head is empty, return false
 const isPalindrome = head => {
   // do something  
 };
