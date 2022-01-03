16a64e5b1ab5ccf99427271758a808bfee3ac3bd
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-02 22:16:19 -0700

    initial pseudocode implemented

--- src/toyProblem126.js
+++ src/toyProblem126.js
@@ -7,7 +7,15 @@ Given the head of a singly linked list, return true if it is a palindrome */
 // constraints: The number of nodes in the list is in the range [1, 105]. 0 <= Node.val <= 9
 // edge cases: if head has one number only, return true. If head is empty, return false
 const isPalindrome = head => {
-  // do something  
+  // account for edge cases
+  // create a result array
+  // while a head is present in linked list
+    // push head of linked list to result array and access next node in linked list
+    // push every next node of linked list to result array until complete
+  // if the reverse of result array equals result array
+    // return true
+  // otherwise
+    // return false
 };
 
 console.log(isPalindrome([1, 2, 2, 1])) // true
