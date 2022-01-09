c687970e0a1bd96eeb93fda0295300aa8d3c604c
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-09 09:54:51 -0700

    initial pseudocode implemented

--- src/toyProblem127.js
+++ src/toyProblem127.js
@@ -9,7 +9,11 @@ A boomerang is a set of three points that are all distinct and not in a straight
 // constraints: points.length == 3. points[i].length == 2. 0 <= xi, yi <= 100
 // edge cases: none at this time
 const isBoomerang = points => {
-  // do something
+  // loop over points
+    // if next index at each y point is NOT current index plus one
+      // return true, this is a boomerang
+    // otherwise
+      // return false, this is not a boomerang
 };
 
 console.log(isBoomerang([[1, 1], [2, 3], [3, 2]])) // true
