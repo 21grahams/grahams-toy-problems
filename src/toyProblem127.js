18bb609901e5539afe82a165b772b3711bc89cc1
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-08 18:48:00 -0700

    test suite implemented

--- src/toyProblem127.js
+++ src/toyProblem127.js
@@ -7,3 +7,6 @@ A boomerang is a set of three points that are all distinct and not in a straight
 const isBoomerang = points => {
   // do something  
 };
+
+console.log(isBoomerang([1,1],[2,3],[3,2])) // true
+console.log(isBoomerang([1,1],[2,2],[3,3])) // false
