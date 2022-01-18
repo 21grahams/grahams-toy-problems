596d372b4987ac1981fda337397b2675d47bb77d
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-17 17:33:11 -0700

    solution implemented and passing all tests

--- src/toyProblem128.js
+++ src/toyProblem128.js
@@ -6,13 +6,25 @@
 // edge cases: none at this time
 const flipPairs = string => {
   // create a flipped string var
+  let flipped = ''
   // split string by every letter
+  let splitter = string.split('')
   // loop over splitter
+  for (let i = 0; i < splitter.length; i++) {
+    if (splitter[i + 1] === undefined) {
+      flipped += splitter[i]
+      break
+    }
     // increment next element to flipped var
+    flipped += splitter[i + 1]
     // increment current element to flipper var
+    flipped += splitter[i]
     // if next element is undefined
-      // increment current element to flipped var
+    // increment current element to flipped var
+    i = i + 1
+  }
   // return flipped
+  return flipped
 }
 
 let testInput = 'check out how interesting this problem is, it\'s insanely interesting!'
