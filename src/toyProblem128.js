6da6877e9fa7e5b200675d96266a32a09bcfafd8
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-17 17:09:35 -0700

    test suite implemented

--- src/toyProblem128.js
+++ src/toyProblem128.js
@@ -3,3 +3,7 @@
 const flipPairs = () => {
   // do something
 }
+
+let testInput = 'check out how interesting this problem is, it\'s insanely interesting!'
+let testOutput = flipPairs(testInput)
+console.log(testOutput) // hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
