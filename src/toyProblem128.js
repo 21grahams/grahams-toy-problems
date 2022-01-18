179606968f2b9c9c108f0bd8798b99373f2a7e36
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-17 17:17:16 -0700

    initial pseudocode implemented

--- src/toyProblem128.js
+++ src/toyProblem128.js
@@ -5,7 +5,14 @@
 // constraints: none at this time
 // edge cases: none at this time
 const flipPairs = string => {
-  // do something
+  // create a flipped string var
+  // split string by every letter
+  // loop over splitter
+    // increment next element to flipped var
+    // increment current element to flipper var
+    // if next element is undefined
+      // increment current element to flipped var
+  // return flipped
 }
 
 let testInput = 'check out how interesting this problem is, it\'s insanely interesting!'
