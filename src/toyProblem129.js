8e996e1dece4fab3eddf8bfbd222affb5a546e03
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-22 17:38:52 -0700

    test case implemented

--- src/toyProblem129.js
+++ src/toyProblem129.js
@@ -1,5 +1,18 @@
 // big flipper - Flip every chunk of n characters in a string, where n is any positive integer greater than 1 
 
-const flipEveryNChars = () => {
+const flipEveryNChars = (string, num) => {
   // do something
 }
+
+let testCase = 'a short example'
+let output = flipEveryNChars(testCase, 5)
+console.log(output) // --> ohs axe trelpma
+
+/*
+Breaking this example down piece by piece:
+'a sho' -> 'ohs a'
+'rt ex' -> 'xe tr'
+'ample' -> 'elpma'
+
+-> 'ohs axe trelpma'
+*/
