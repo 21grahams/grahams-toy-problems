8ccec347db3b0e88df0a45507ad05e5302ee4363
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-01-23 23:10:38 -0700

    IOCE implemented

--- src/toyProblem129.js
+++ src/toyProblem129.js
@@ -1,5 +1,9 @@
 // big flipper - Flip every chunk of n characters in a string, where n is any positive integer greater than 1 
 
+// input: a string of characters and a number (integer)
+// output: that string, with chunks of characters flipped, by a rate of the given input integer
+// constraints: none at this time
+// edge cases: none at this time
 const flipEveryNChars = (string, num) => {
   // do something
 }
