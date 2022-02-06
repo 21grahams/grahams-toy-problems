111ae3e9a2e5b8249b0b1568e9742a57fa4aaa0a
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-02-06 09:55:04 -0700

    test suite implemented

--- src/toyProblem130.js
+++ src/toyProblem130.js
@@ -10,3 +10,6 @@ detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest 
 const detectOutlierValue = input => {
   // do something
 }
+
+console.log(detectOutlierValue('2 4 7 8 10')) // 3 Third number is odd, while the rest of the numbers are even
+console.log(detectOutlierValue('1 10 1 1')) // 2 Second number is even, while the rest of the numbers are odd
