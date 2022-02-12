d2f25fcc7dac4195c41fecd1e783641d1485756d
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-02-12 09:24:42 -0700

    IOCE implemented

--- src/toyProblem130.js
+++ src/toyProblem130.js
@@ -7,6 +7,10 @@ detectOutlierValue("2 4 7 8 10"); // => 3 - Third number is odd, while the rest 
 detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest of the numbers are odd
 */
 
+// input: a string of even and / or odd numbers
+// output: an integer, which represents the index of the number out of place (even or odd depending on input)
+// constraints: none at this time
+// edge cases: none at this time
 const detectOutlierValue = input => {
   // do something
 }
