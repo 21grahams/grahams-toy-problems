a85c4457adc3aaaae851093d6ebc6e89dec07f68
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-02-13 14:55:42 -0700

    initial pseudocode for brute force solution

--- src/toyProblem130.js
+++ src/toyProblem130.js
@@ -12,7 +12,19 @@ detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest 
 // constraints: none at this time
 // edge cases: none at this time
 const detectOutlierValue = input => {
-  // do something
+  // create a count variable, initialize to 0
+  // create a boolean flag, set to false
+  // loop over input
+    // if current element is odd
+      // flip bool to true
+    // if any other elements are odd
+      // break
+    // else
+      // assign count to current element plus one
+
+    // repeat process of lines 18 through 23 for even
+
+  // return count
 }
 
 console.log(detectOutlierValue('2 4 7 8 10')) // 3 Third number is odd, while the rest of the numbers are even
