813016a8b7c076389acb2869bd93037727fa9e80
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-02-19 20:52:21 -0700

    toy problem init - transpose

---
+++ src/toyProblem131.js
@@ -0,0 +1,19 @@
+/* Transpose
+
+/* You will be given an array that contains two strings. Your job is to create a function that will take those two strings and transpose them, so that the strings go from top to bottom instead of left to right.
+
+e.g. transposeTwoStrings(['Hello','World']);
+
+should return:
+H W  
+e o  
+l r  
+l l  
+o d
+t h
+f
+*/
+
+const transposeTwoStrings = (str1, str2) => {
+  // do something
+}
