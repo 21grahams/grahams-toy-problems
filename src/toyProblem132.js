b8b44dab4e13b3888a57c19d9df676a23157de65
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-02-26 21:50:10 -0700

    toyProblem init - findPairsForSum

---
+++ src/toyProblem132.js
@@ -0,0 +1,14 @@
+/* Given a list of non-negative integers and a target sum, find a pair of numbers that sums to the target sum.
+
+Example:
+
+var pair = findPairForSum([3, 34, 4, 12, 5, 2], 9);
+console.log(pair); // --> [4, 5]
+*/
+
+// input: array of numbers and a target number
+// output: array of a pair of numbers
+
+function findPairForSum(integers, targetSum) {
+  // do something
+}
