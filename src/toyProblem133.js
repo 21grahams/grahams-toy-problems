518cbb8b6c3de9d225d77b20530b151b3c05b98e
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-03-13 20:28:01 -0600

    test suite implemented

--- src/toyProblem133.js
+++ src/toyProblem133.js
@@ -6,3 +6,8 @@ given an array, move all zeroes to the back
 const moveZeroes = () => {
   // do something
 }
+
+console.log(moveZeroes([1, 0, 2, 3, 0, 5, 5, 35, 0])) // [1, 2, 3, 5, 5, 35, 0, 0, 0]
+console.log(moveZeroes([9, 8, 7, 6, 5, 0, 0, 0, 0, 1])) // [9, 8, 6, 5, 5, 1, 0, 0, 0, 0]
+console.log(moveZeroes([0, 1, 1])) // [1, 1, 0]
+console.log(moveZeroes([])) // []
