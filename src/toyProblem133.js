e5c17de942300f7ded134661a5ec6feccf27f5d3
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-03-20 18:29:34 -0600

    solution implemented and passing all tests

--- src/toyProblem133.js
+++ src/toyProblem133.js
@@ -11,21 +11,23 @@ const moveZeroes = nums => {
   // account for edge case
   if (nums.length === 0) return []
   // create a result array 
-  let count = 0
+  let result = []
   // loop over nums array
   for (let i = 0; i < nums.length; i++) {
     // if current num in array is a 0
     if (nums[i] === 0) {
       // push current element to result array
-      count++
+      result.push(nums[i])
       // splice out 0
       nums.splice(i, 1)
       // decrement index
       i--
     }
   }
-  // concat result array with nums array
+  // spread result array with nums array
+  nums.push(...result)
   // return nums array
+  return nums
 }
 
 console.log(moveZeroes([1, 0, 2, 3, 0, 5, 5, 35, 0])) // [1, 2, 3, 5, 5, 35, 0, 0, 0]
