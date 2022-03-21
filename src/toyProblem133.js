e4d754796ba6c0bee09a2f09b66a3ac539ebf190
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-03-20 18:30:14 -0600

    missed item in test case

--- src/toyProblem133.js
+++ src/toyProblem133.js
@@ -31,6 +31,6 @@ const moveZeroes = nums => {
 }
 
 console.log(moveZeroes([1, 0, 2, 3, 0, 5, 5, 35, 0])) // [1, 2, 3, 5, 5, 35, 0, 0, 0]
-console.log(moveZeroes([9, 8, 7, 6, 5, 0, 0, 0, 0, 1])) // [9, 8, 6, 5, 5, 1, 0, 0, 0, 0]
+console.log(moveZeroes([9, 8, 7, 6, 5, 0, 0, 0, 0, 1])) // [9, 8, 7, 6, 5, 5, 1, 0, 0, 0, 0]
 console.log(moveZeroes([0, 1, 1])) // [1, 1, 0]
 console.log(moveZeroes([])) // []
