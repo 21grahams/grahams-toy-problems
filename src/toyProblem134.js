dffe0c7e9ce3d6507488678909392538bec9d680
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-03-26 22:34:30 -0600

    test suite implemented

--- src/toyProblem134.js
+++ src/toyProblem134.js
@@ -9,3 +9,12 @@ Return the sum of all the unique elements of nums.
 const sumOfUnique = nums => {
   // do something
 };
+
+console.log(sumOfUnique([1, 2, 3, 2])) // 4
+// Explanation: The unique elements are [1,3], and the sum is 4.
+
+console.log(sumOfUnique([1,1,1,1,1])) // 0
+//Eplanation: There are no unique elements, and the sum is 0.
+
+console.log(sumOfUnique([1,2,3,4,5])) // 15
+// Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
