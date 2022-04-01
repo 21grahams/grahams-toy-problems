05a54bf371f3f1bd9672b273bf05c105c84a87b0
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-04-01 16:54:19 -0600

    leetcode init - Check If Two String Arrays are Equivalent

---
+++ src/toyProblem135.js
@@ -0,0 +1,11 @@
+/*
+leetcode - Check If Two String Arrays are Equivalent
+
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated in order forms the string.
+*/
+
+const arrayStringsAreEqual = (word1, word2) => {
+  // do something   
+}
