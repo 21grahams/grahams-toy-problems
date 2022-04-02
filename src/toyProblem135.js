f158bcc283ae1df62ae7640213c48aa1de1db3d0
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-04-02 09:13:55 -0600

    test suite implemented

--- src/toyProblem135.js
+++ src/toyProblem135.js
@@ -9,3 +9,10 @@ A string is represented by an array if the array elements concatenated in order 
 const arrayStringsAreEqual = (word1, word2) => {
   // do something   
 }
+
+console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"])) // true
+// Explanation: word1 represents string "ab" + "c" -> "abc". word2 represents string "a" + "bc" -> "abc". The strings are the same, so return true.
+
+console.log(arrayStringsAreEqual(["a", "cb"], ["ab", "c"])) // false
+
+console.log(arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"])) // true
