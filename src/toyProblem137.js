c45263fb0f131a64c0604ac09cd45d34d49e48ab
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-05-07 16:23:31 -0600

    test suite implemented

--- src/toyProblem137.js
+++ src/toyProblem137.js
@@ -8,3 +8,8 @@ Given a string date representing a Gregorian calendar date formatted as YYYY-MM-
 const dayOfYear = date => {
   // do something
 }
+
+console.log(dayOfYear('2019-01-09')) // 9
+// Explanation: Given date is the 9th day of the year in 2019.
+
+console.log(dayOfYear('2019-02-10')) // 41
