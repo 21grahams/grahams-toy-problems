0f420bf432c92a5cc90cce4c2ae1a6185498c350
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-05-07 16:22:35 -0600

    leetcode init - Day of the Year

---
+++ src/toyProblem137.js
@@ -0,0 +1,10 @@
+/*
+leetcode - Day of the Year
+
+Given a string date representing a Gregorian calendar date formatted as YYYY-MM-DD, return the day number of the year.
+
+*/
+
+const dayOfYear = date => {
+  // do something
+}
