e22fb1564f96d5f521baf6b10307be9ab429a08e
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-05-15 20:08:57 -0600

    leetcode init - Unique Number of Occurrences

---
+++ src/toyProblem138.js
@@ -0,0 +1,9 @@
+/*
+leetcode - Unique Number of Occurrences
+
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique, or false otherwise.
+*/
+
+const uniqueOccurrences = arr => {
+  // do something
+}
