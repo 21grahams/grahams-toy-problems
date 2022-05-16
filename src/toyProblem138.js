dd357e88d147129cc852b6ac745e19ad2113bc1c
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-05-15 20:10:39 -0600

    test suite implemented

--- src/toyProblem138.js
+++ src/toyProblem138.js
@@ -7,3 +7,10 @@ Given an array of integers arr, return true if the number of occurrences of each
 const uniqueOccurrences = arr => {
   // do something
 }
+
+console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3])) // true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+console.log(uniqueOccurrences([1, 2])) // false
+
+console.log(uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0])) // true
