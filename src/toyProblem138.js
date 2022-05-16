9d03d4170d0dc6bb772b5152886722c825dbe373
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-05-15 20:37:16 -0600

    runtime 78ms, faster than 53% of online submissions

--- src/toyProblem138.js
+++ src/toyProblem138.js
@@ -36,6 +36,9 @@ const uniqueOccurrences = (arr) => {
   return true
 };
 
+// Runtime: 78 ms, faster than 53.20% of JavaScript online submissions for Unique Number of Occurrences.
+// Memory Usage: 42.9 MB, less than 43.19% of JavaScript online submissions for Unique Number of Occurrences.
+
 console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3])); // true
 // Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
 
