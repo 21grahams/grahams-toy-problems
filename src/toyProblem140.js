d685970ac2e0d866b1bb2b61083fecb667bfeba4
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-06-18 11:55:24 -0600

    code implemented and passing test cases here

--- src/toyProblem140.js
+++ src/toyProblem140.js
@@ -12,21 +12,22 @@ A substring is a contiguous sequence of characters within a string
 // constraints: 1 <= s.length <= 300. s contains only lowercase English letters.
 // edge cases: If there is no such substring return -1
 const maxLengthBetweenEqualCharacters = s => {
-  // create a count var, assign to 0
-  // create an object
-  // loop over the input string
-    // if the current element hasn't been seen
-      // assign the object at that value to that letter
-    // otherwise 
-      // incremement the value
-  
-  // loop over the object
-    // if every value is only seen once
-      // return -1 (edge case)
-    // else if there's only one value
-      // return 0
-    // else if one value has more than one
-      // return the value of the other numbers
+  let count = 0, beenSeen = false, resultObj = {}
+  for (let i = 0; i < s.length; i++) {
+    if (resultObj[s[i]] === undefined) {
+      resultObj[s[i]] = 1
+    } else {
+      resultObj[s[i]]++
+      beenSeen = true
+    }
+    if (s[i + 1] !== s[i] && !beenSeen && i !== 0) {
+      count++
+      continue
+    }
+    
+  }
+  if (!beenSeen) count = -1
+  return count
 };
 
 console.log(maxLengthBetweenEqualCharacters('aa')) // 0
