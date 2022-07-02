6ca2d33e189bbbf474d3464a22b4c22717ed805f
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-02 16:14:28 -0600

    added test case, removed pseudocode

--- src/toyProblem140.js
+++ src/toyProblem140.js
@@ -11,20 +11,20 @@ A substring is a contiguous sequence of characters within a string
 // output: a number, which equals the longest substrings between two equal chars
 // constraints: 1 <= s.length <= 300. s contains only lowercase English letters.
 // edge cases: If there is no such substring return -1
-const maxLengthBetweenEqualCharacters = s => {
+const maxLengthBetweenEqualCharacters = (s) => {};
 
-};
-
-console.log(maxLengthBetweenEqualCharacters('aa')) // 0
+console.log(maxLengthBetweenEqualCharacters("aa")); // 0
 // Explanation: The optimal substring here is an empty substring between the two 'a's.
 
-console.log(maxLengthBetweenEqualCharacters('abca')) // 2
-debugger
+console.log(maxLengthBetweenEqualCharacters("abca")); // 2
+debugger;
 // Explanation: The optimal substring here is "bc".
 
-console.log(maxLengthBetweenEqualCharacters('cbzxy')) // -1
+console.log(maxLengthBetweenEqualCharacters("cbzxy")); // -1
 // Explanation: There are no characters that appear twice in s.
 
-console.log(maxLengthBetweenEqualCharacters('cabbac')) // 4
+console.log(maxLengthBetweenEqualCharacters("cabbac")); // 4
 
-console.log(maxLengthBetweenEqualCharacters('scayofdzca')) // 6
+console.log(maxLengthBetweenEqualCharacters("scayofdzca")); // 6
+
+console.log(maxLengthBetweenEqualCharacters("mgntdygtxrvxjnwksqhxuxtrv")); // 18
