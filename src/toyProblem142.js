4aedee8a2bec9581683468b65a6332b2d658e82b
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-04 14:04:30 -0600

    test suite implemented

--- src/toyProblem142.js
+++ src/toyProblem142.js
@@ -6,6 +6,20 @@ Write a function to delete a node in a singly-linked list. You will not be given
 It is guaranteed that the node to be deleted is not a tail node in the list.
 */
 
+var Node = function (val, next) {
+  this.val = val;
+  this.next = null;
+};
+
 const deleteNode = node => {
   // do something  
 };
+
+let nodeA = new Node(4)
+let nodeB = new Node(5)
+let nodeC = new Node(1)
+let nodeD = new Node(9)
+nodeA.next = nodeB
+nodeB.next = nodeC
+nodeC.next = nodeD
+nodeD.next = null
