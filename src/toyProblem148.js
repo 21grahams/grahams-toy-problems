997b7aa005b8a893d44a154ad22163367ed4866d
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-18 20:35:28 -0600

    leetcode init - Longest Consecutive Sequence

---
+++ src/toyProblem148.js
@@ -0,0 +1,11 @@
+/*
+leetcode - Longest Consecutive Sequence
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+*/
+
+const longestConsecutive = nums => {
+  // do something
+}
