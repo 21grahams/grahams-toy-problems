81b058cb0e989dc55659af7d40e852b9c6fa04ef
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-18 20:36:38 -0600

    test suite implemented

--- src/toyProblem148.js
+++ src/toyProblem148.js
@@ -9,3 +9,8 @@ You must write an algorithm that runs in O(n) time.
 const longestConsecutive = nums => {
   // do something
 }
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2])) // 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])) // 9
