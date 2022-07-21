67b5e38d48c3ad486df97b7a72c5caa45d237d88
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-20 22:16:45 -0600

    reset. Added test case and sequencing

--- src/toyProblem148.js
+++ src/toyProblem148.js
@@ -10,20 +10,16 @@ You must write an algorithm that runs in O(n) time.
 // input: an array of unsorted integers
 // output: an integer which represents the longest consecutive element sequence of the input array
 // constraints: Must create an algorithm that runs in linear time complexity. 0 <= nums.length <= 105. -109 <= nums[i] <= 109
-// edgecases: if array is empty, return null
+// edgecases: if array is empty, return 0
 const longestConsecutive = nums => {
-  // account for edge case
-  // sort nums input array
-  // create a count, init to 0
-  // loop over nums array
-    // if current element plus one DOESN'T equal next element
-      // assign count to 0
-    // if current element plus one equals next element
-      // increment count
-  // return count
+  if (nums.length === 0) return 0
 }
 
 console.log(longestConsecutive([100, 4, 200, 1, 3, 2])) // 4
 // Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 
 console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])) // 9
+// 0, 0, 1, 2, 3, 4, 5, 6, 7, 8
+
+console.log(longestConsecutive([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6])) // 7
+// -1, -1, 0, 1, 3, 4, 5, 6, 7, 8, 9
