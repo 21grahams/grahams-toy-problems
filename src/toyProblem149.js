ace2c3d7a978038a3d979c2f3bb5919b2c58fd2f
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-23 22:46:41 -0600

    leetcode init - transpose matrix

---
+++ src/toyProblem149.js
@@ -0,0 +1,11 @@
+/*
+leetcode - transpose matrix
+
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+*/
+
+const transpose = matrix => {
+  // do something
+}
