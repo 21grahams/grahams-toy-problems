67e407495303bd844d5fabfb4710e9a545880ae4
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-23 22:51:58 -0600

    test suite implemented

--- src/toyProblem149.js
+++ src/toyProblem149.js
@@ -9,3 +9,7 @@ The transpose of a matrix is the matrix flipped over its main diagonal, switchin
 const transpose = matrix => {
   // do something
 }
+
+console.log(transpose([[1, 2, 3], [4, 5, 6], [7, 8, 9]])) // [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+console.log(transpose([[1, 2, 3], [4, 5, 6]])) // [[1, 4], [2, 5], [3, 6]]
