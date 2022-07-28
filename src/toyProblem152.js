5108ceda703510d3875a399d729d9aae75582177
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-27 22:31:09 -0600

    solution implemented and passing all leetcode tests

--- src/toyProblem152.js
+++ src/toyProblem152.js
@@ -12,21 +12,26 @@ Return the number of matches played in the tournament until a winner is decided.
 // output: an integer which represents the total number of matches played in the tournament once a winner was decided
 // constraints: aside from the constraints above: 1 <= n <= 200
 // edge cases: none at this time
-const numberOfMatches = n => {
-  // RECURSION 
+const numberOfMatches = (n) => {
+  let matches;
+  if (n === 1) return 0;
 
-  // base case
-  // if n is equal to 1
-    // return 1
+  // EVEN
+  if (n % 2 === 0) {
+    matches = n / 2;
+    n = n - matches;
+    return matches + numberOfMatches(n);
 
-  // recursion case
-  // if n is an even number
-    // call our recursion func with n / 2
-  // otherwise 
-    // call our recursion func with (n - 1) / 2
-}
+    // ODD
+  } else {
+    matches = (n - 1) / 2;
+    n = n - matches;
+    return matches + numberOfMatches(n)
+  }
+  return matches
+};
 
-console.log(numberOfMatches(7)) // 6
+console.log(numberOfMatches(7)); // 6
 /* Explanation: Details of the tournament: 
 - 1st Round: Teams = 7, Matches = 3, and 4 teams advance.
 - 2nd Round: Teams = 4, Matches = 2, and 2 teams advance.
@@ -34,7 +39,7 @@ console.log(numberOfMatches(7)) // 6
 Total number of matches = 3 + 2 + 1 = 6.
 */
 
-console.log(numberOfMatches(14)) // 13
+console.log(numberOfMatches(14)); // 13
 /* Explanation: Details of the tournament:
 - 1st Round: Teams = 14, Matches = 7, and 7 teams advance.
 - 2nd Round: Teams = 7, Matches = 3, and 4 teams advance.
