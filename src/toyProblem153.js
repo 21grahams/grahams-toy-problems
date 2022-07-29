76d1a841c1e509bc581f074d7de1750b80c7a278
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-28 20:59:05 -0600

    test suite implemented

--- src/toyProblem153.js
+++ src/toyProblem153.js
@@ -9,3 +9,17 @@ Return the maximum product of nums after at most k operations. Since the answer 
 const maximumProduct = (nums, k) => {
   // do something
 }
+
+console.log(maximumProduct([0, 4], 5)) // 20
+/* Explanation: Increment the first number 5 times.
+Now nums = [5, 4], with a product of 5 * 4 = 20.
+It can be shown that 20 is maximum product possible, so we return 20.
+Note that there may be other ways to increment nums to have the maximum product.
+*/
+
+console.log(maximumProduct([6, 3, 3, 2], 2)) // 216
+/* Explanation: Increment the second number 1 time and increment the fourth number 1 time.
+Now nums = [6, 4, 3, 3], with a product of 6 * 4 * 3 * 3 = 216.
+It can be shown that 216 is maximum product possible, so we return 216.
+Note that there may be other ways to increment nums to have the maximum product.
+*/
