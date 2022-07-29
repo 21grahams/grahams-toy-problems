98ae87745a30889d825c403c0e36c0cc3836a57f
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-28 21:04:53 -0600

    IOCE implemented

--- src/toyProblem153.js
+++ src/toyProblem153.js
@@ -3,9 +3,13 @@ leetcode - Maximum Product After K Increments
 
 You are given an array of non-negative integers nums and an integer k. In one operation, you may choose any element from nums and increment it by 1.
 
-Return the maximum product of nums after at most k operations. Since the answer may be very large, return it modulo 109 + 7. Note that you should maximize the product before taking the modulo. 
+Return the maximum product of nums after at most k operations. Since the answer may be very large, return it modulo 10^9 + 7. Note that you should maximize the product before taking the modulo. 
 */
 
+// input: an array of non negative integers and an integer (k) which represents the amount you can increment any of the nums in array
+// output: a single integer, representing the largest possible product of the nums in the input array
+// constraints: 1 <= nums.length, k <= 105. 0 <= nums[i] <= 106
+// edge cases: not necessarily an edge case but ensure no number stays at 0, or product becomes 0
 const maximumProduct = (nums, k) => {
   // do something
 }
