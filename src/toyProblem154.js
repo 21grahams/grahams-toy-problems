d028b89d24bb7f7b6d16a39e368c88304a5b2a0d
21grahams <graham.kirsh@ext.myfitnesspal.com>
2022-07-31 20:27:39 -0600

    initial pseudocode implemented

--- src/toyProblem154.js
+++ src/toyProblem154.js
@@ -12,8 +12,18 @@ You must solve the problem without modifying the array nums and uses only consta
 // output: a single integer which represents the duplicate number from the input array
 // constraints: 1 <= n <= 105. nums.length == n + 1. 1 <= nums[i] <= n. All the integers in nums appear only once except for precisely one integer which appears two or more times.
 // edge cases: if the array is empty, return null
-const findDuplicate = () => {
-  // do something
+const findDuplicate = (nums) => {
+  // create a mapping obj
+  // create a finalCount var
+  // loop over nums
+    // if the current num at the object is undefined
+      // assign current num at the object to 1
+    // otherwise
+      // increment it
+  // loop over the object
+    // if the value at the key is greater than 1
+      // assign finalCount to current key 
+  // return finalCount
 }
 
 console.log(findDuplicate([1, 3, 4, 2, 2])) // 2
